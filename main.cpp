48d745b6ee2eaef0c007d254e67ab735a90c46a6
veggiesaurus <accomrie@gmail.com>
2017-08-23 17:15:26 +0200

    using HighFive HDF5 C++ wrapper and libfmt

--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
-#include <hdf5.h>
+#include <vector>
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+#include <highfive/H5File.hpp>
 
 using namespace std;
+using namespace HighFive;
 
 int main()
 {
-	cout << "Hello, World!" << std::endl;
-	auto file_id = H5Fcreate("file.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
-	auto status = H5Fclose(file_id);
-	cout<<status<<endl;
+	File file("L13591_sky.h5", File::ReadOnly);
+	Group group = file.getGroup("Image001/skyData");
+	DataSet dataSet = group.getDataSet("ImageDataArray_SB001");
+	DataSpace dataSpace = dataSet.getSpace();
+	auto dims = dataSpace.getDimensions();
+	fmt::print("Rank {} (", dims.size());
+	size_t index = 0;
+	for (auto dim: dims)
+	{
+		if (++index != dims.size())
+			fmt::print("{} x ", dim);
+		else
+			fmt::print("{})\n", dim);
+	}
+
 	return 0;
 }
