8b498b227525ead82cdbda2f7403962b5267f40e
veggiesaurus <accomrie@gmail.com>
2018-02-21 09:55:33 +0200

    size_t definition fix

--- compression.h
+++ compression.h
@@ -2,7 +2,7 @@
 
 #include <vector>
 #include <cstdint>
-
+#include <cstddef>
 int compress(std::vector<float>& array, std::vector<char>& compressionBuffer, size_t& zfpsize, uint32_t nx, uint32_t ny, uint32_t precision);
 int decompress(std::vector<float>& array, std::vector<char>& compressionBuffer, size_t& zfpsize, uint32_t nx, uint32_t ny, uint32_t precision);
 std::vector<int32_t> getNanEncodings(std::vector<float>& array);
