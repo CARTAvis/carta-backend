33c4f76c27f04d2737b26aa0362624cf9ab061ae
ajm-asiaa <ajm@asiaa.sinica.edu.tw>
2018-11-26 16:36:24 +0800

    removing the need for boost in casacore

--- scripts/ci_mac_casacore.sh
+++ scripts/ci_mac_casacore.sh
@@ -13,15 +13,20 @@ mv casacore casacore-src
 
 cd casacore-src && mkdir build && cd build
 
+# Boost should no longer be necessary
 #Fix so that cmake can find the boost-python library file
-ln -s /usr/local/Cellar/boost-python/1.67.0/lib/libboost_python27-mt.dylib \
-      /usr/local/lib/libboost_python-mt.dylib
+#ln -s /usr/local/Cellar/boost-python/1.68.0/lib/libboost_python27-mt.dylib \
+#      /usr/local/lib/libboost_python-mt.dylib
 
 cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/casacore \
          -DUSE_FFTW3=ON \
          -DUSE_HDF5=ON \
          -DUSE_THREADS=ON \
          -DUSE_OPENMP=ON \
+         -DCMAKE_BUILD_TYPE=Release \
+         -DBUILD_TESTING=OFF \
+         -DBoost_NO_BOOST_CMAKE=1 \
+         -DBUILD_PYTHON=OFF \
          -DDATA_DIR="%CASAROOT%/data"
 make -j 2
 
