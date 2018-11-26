d8cc4de7b64fe057d7adb3f3a95a718204001f7e
Anthony Moraghan <ajm@asiaa.sinica.edu.tw>
2018-11-26 15:39:06 +0800

    adding a new Travis build stage that builds the frontend and runs a simple jest test

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,5 +9,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )/../etc"
 
 export CASAPATH="../../../../$DIR linux"
 
-./carta_backend folder=$HOME/CARTA/Images
-
+#./carta_backend folder=$HOME/CARTA/Images
+./carta_backend
