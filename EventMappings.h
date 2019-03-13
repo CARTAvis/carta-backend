27c0fe9aaf350038c2a929cca8fdb0aa1e158c7b
Rob Simmonds <robert.simmonds@uct.ac.za>
2019-03-13 09:49:39 +0200

    Mapping from message string name to event ids

---
+++ EventMappings.h
@@ -0,0 +1,15 @@
+#pragma once
+
+const uint32_t REGISTER_VIEWER_ID= 1;
+const uint32_t FILE_LIST_REQUEST_ID= 2;
+const uint32_t FILE_INFO_REQUEST_ID= 3;
+const uint32_t OPEN_FILE_ID= 4;
+const uint32_t SET_IMAGE_VIEW_ID= 5;
+const uint32_t SET_IMAGE_CHANNELS_ID= 6;
+const uint32_t SET_CURSOR_ID= 7;
+const uint32_t SET_SPATIAL_REQUIREMENTS_ID= 8;
+const uint32_t SET_HISTOGRAM_REQUIREMENTS_ID= 9;
+const uint32_t SET_STATS_REQUIREMENTS_ID= 10;
+const uint32_t SET_REGION_ID= 11;
+const uint32_t REMOVE_REGION_ID= 12;
+const uint32_t CLOSE_FILE_ID= 13;
