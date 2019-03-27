a821320c671a168732ad0a42cef334c941d3b67b
Rob Simmonds <robert.simmonds@uct.ac.za>
2019-03-27 19:14:18 +0200

    Fixed conflicts.

--- Session.cc
+++ Session.cc
@@ -17,15 +17,23 @@ using namespace std;
 
 int __num_sessions= 0;
 // Default constructor. Associates a websocket with a UUID and sets the root and base folders for all files
-Session::Session(uWS::WebSocket<uWS::SERVER>* ws, std::string uuid, std::string root,
-    uS::Async *outgoing, FileListHandler *fileListHandler, bool verbose)
+Session::Session(uWS::WebSocket<uWS::SERVER>* ws,
+		 std::string uuid,
+		 std::unordered_map<std::string, std::vector<std::string>>& permissionsMap,
+		 bool enforcePermissions,
+		 std::string root,
+		 std::string base, // is currently ignored ???
+		 uS::Async *outgoing_,
+		 FileListHandler *fileListHandler,
+		 bool verbose)
     : uuid(std::move(uuid)),
       socket(ws),
       rootFolder(root),
+      baseFolder(base),
       verboseLogging(verbose),
       selectedFileInfo(nullptr),
       selectedFileInfoExtended(nullptr),
-      outgoing(outgoing),
+      outgoing(outgoing_),
       fileListHandler(fileListHandler),
       newFrame(false),
       fsettings(this) {
