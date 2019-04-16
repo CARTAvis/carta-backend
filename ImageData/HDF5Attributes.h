4abfc2d6dbbcf7f0d17bf0a24f2916f13173fe17
Pam Ford <pford@nrao.edu>
2019-04-16 15:40:50 -0600

    Moved functions to get numerical HDF5 attributes
    Moved out of FileInfoLoader to HDF5Attributes, needed for HDF5Image

--- ImageData/HDF5Attributes.h
+++ ImageData/HDF5Attributes.h
@@ -13,6 +13,12 @@ public:
     // (links get HDF5Error "Could not open group XXX in parent")
     static casacore::Record doReadAttributes(hid_t groupHid);
 
+    // These attributes may be string type instead of numerical type
+    static bool getIntAttribute(casacore::Int64& val, const casacore::Record& rec,
+        const casacore::String& field);
+    static bool getDoubleAttribute(casacore::Double& val, const casacore::Record& rec,
+        const casacore::String& field);
+
 private:
     // Read a scalar value (int, float, string) and add it to the record.
     static void readScalar (hid_t attrId, hid_t dtid, const casacore::String& name,
