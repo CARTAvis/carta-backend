189947fb002563eef288452d306f7b1ddc9a02d7
Adrianna Pińska <adrianna.pinska@gmail.com>
2019-05-10 15:53:37 +0200

    Initial clang-tidy config + utility scripts (#213)
    * export compile commands from cmake
    
    * starting clang-tidy config plus (read-only) wrapper script
    
    * added script for checking if formatting is necessary. Fixed reformat script to ignore autogenerated files in the build directory.
    
    * Changed function case to CamelCase.

---
+++ scripts/checkstyle.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+
+# This is a utility script for running clang-tidy on all files to check for style issues.
+# This does not modify the files.
+# It should be run from the root directory of the repository.
+# The clang-tidy configuration is found in .clang-tidy
+
+run-clang-tidy -p build
