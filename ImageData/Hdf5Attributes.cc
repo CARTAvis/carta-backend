9603f56a024ad96d5d0161fdd150ab5fe9dc3c9d
Pam Ford <pford@nrao.edu>
2019-05-15 16:53:44 -0600

    Convert additional hdf5 headers from string to numeric or bool

--- ImageData/Hdf5Attributes.cc
+++ ImageData/Hdf5Attributes.cc
@@ -5,14 +5,14 @@
 
 #include <casacore/casa/HDF5/HDF5DataType.h>
 #include <casacore/casa/HDF5/HDF5Error.h>
+#include <casacore/fits/FITS/FITSDateUtil.h>
 
 casacore::Record Hdf5Attributes::ReadAttributes(hid_t group_hid) {
     // reads attributes but not links
-    casacore::Record rec;
+    casacore::Record attributes_record;
     char cname[512];
     int num_fields = H5Aget_num_attrs(group_hid);
-    // Iterate through the attributes in order of index, so we're sure
-    // they are read back in the same order as written.
+    // Iterate through the attributes in order of index, so we're sure they are read back in the same order as written.
     for (int index = 0; index < num_fields; ++index) {
         casacore::HDF5HidAttribute id(H5Aopen_idx(group_hid, index));
         AlwaysAssert(id >= 0, casacore::AipsError);
@@ -30,11 +30,12 @@ casacore::Record Hdf5Attributes::ReadAttributes(hid_t group_hid) {
         // Get data type and its size.
         if (rank == 0) {
             casacore::HDF5HidDataType dtid(H5Aget_type(id));
-            ReadScalar(id, dtid, name, rec);
+            ReadScalar(id, dtid, name, attributes_record);
         }
         H5Aclose(id);
     }
-    return rec;
+    ConvertAttributeStrings(attributes_record);
+    return attributes_record;
 }
 
 void Hdf5Attributes::ReadScalar(hid_t attr_id, hid_t data_type_id, const casacore::String& name, casacore::RecordInterface& rec) {
@@ -66,41 +67,59 @@ void Hdf5Attributes::ReadScalar(hid_t attr_id, hid_t data_type_id, const casacor
     }
 }
 
-// get int value (might be string)
-bool Hdf5Attributes::GetIntAttribute(casacore::Int64& val, const casacore::Record& rec, const casacore::String& field) {
-    bool get_ok(true);
-    if (rec.isDefined(field)) {
-        try {
-            val = rec.asInt64(field);
-        } catch (casacore::AipsError& err) {
-            try {
-                val = casacore::String::toInt(rec.asString(field));
-            } catch (casacore::AipsError& err) {
-                get_ok = false;
-            }
-        }
-    } else {
-        get_ok = false;
-    }
-    return get_ok;
-}
+void Hdf5Attributes::ConvertAttributeStrings(casacore::Record& attributes) {
+    // convert specified keywords to bool, int, or double
+    std::vector<std::string> skip_entries{"SCHEMA_VERSION", "HDF5_CONVERTER", "HDF5_CONVERTER_VERSION"};
+    std::vector<std::string> int_entries{"BITPIX", "WCSAXES", "A_ORDER", "B_ORDER"};
+    std::vector<std::string> double_entries{"EQUINOX", "EPOCH", "LONPOLE", "LATPOLE", "RESTFRQ", "OBSFREQ", "MJD-OBS", "DATAMIN", "DATAMAX",
+                                           "BMAJ", "BMIN", "BPA"};
+    std::vector<std::string> substr_entries{"CRVAL", "CRPIX", "CDELT", "CROTA"};
+    std::vector<std::string> prefix_entries{"A_", "B_", "CD"};
 
-// get double value (might be string)
-bool Hdf5Attributes::GetDoubleAttribute(casacore::Double& val, const casacore::Record& rec, const casacore::String& field) {
-    bool get_ok(true);
-    if (rec.isDefined(field)) {
-        try {
-            val = rec.asDouble(field);
-        } catch (casacore::AipsError& err) {
-            try {
-                val = casacore::String::toDouble(rec.asString(field));
-            } catch (casacore::AipsError& err) {
-                get_ok = false;
-            }
-        }
-    } else {
-        get_ok = false;
-    }
-    return get_ok;
-}
+    casacore::Record header_record;
+    for (int i = 0; i < attributes.nfields(); ++i) {
+        const std::string entry_name = attributes.name(i);
+        const std::string entry_name_substr = entry_name.substr(0,5);
+        const std::string entry_name_prefix = entry_name.substr(0,2);
 
+        if (std::find(skip_entries.begin(), skip_entries.end(), entry_name) != skip_entries.end()) {
+            continue;
+        }
+
+        switch (attributes.type(i)) {
+            case casacore::DataType::TpString: {
+                casacore::String entry_value = attributes.asString(i);
+                if ((entry_name == "SIMPLE") || (entry_name == "EXTEND") || (entry_name == "BLOCKED")) { // bool
+                    header_record.define(entry_name, (entry_value == "T" ? 1 : 0));
+                } else if ((std::find(int_entries.begin(), int_entries.end(), entry_name) != int_entries.end()) ||
+                           (entry_name_substr == "NAXIS")) {
+                    header_record.define(entry_name, std::stoi(entry_value)); // int
+                } else if (std::find(double_entries.begin(), double_entries.end(), entry_name) != double_entries.end()) {
+                    header_record.define(entry_name, std::stod(entry_value)); // double
+                } else if (std::find(substr_entries.begin(), substr_entries.end(), entry_name_substr) != substr_entries.end()) {
+                    header_record.define(entry_name, std::stod(entry_value)); // double
+                } else if (std::find(prefix_entries.begin(), prefix_entries.end(), entry_name_prefix) != prefix_entries.end()) {
+                    header_record.define(entry_name, std::stod(entry_value)); // double
+                } else {
+                    entry_value = (entry_value == "Kelvin" ? "K" : entry_value);
+                    if (entry_name == "DATE-OBS") { // date
+                        casacore::String fits_date;
+                        casacore::FITSDateUtil::convertDateString(fits_date, entry_value);
+                        header_record.define(entry_name, fits_date);
+                    } else {
+                        header_record.define(entry_name, entry_value); // string
+                    }
+                }
+            } break;
+            case casacore::DataType::TpInt64: {
+                header_record.define(entry_name, attributes.asInt64(i));
+            } break;
+            case casacore::DataType::TpDouble: {
+                header_record.define(entry_name, attributes.asDouble(i));
+            } break;
+            default:
+                break;
+        }
+    }
+    attributes = header_record;
+}
