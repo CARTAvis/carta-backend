12e9019172dbde56a448e4eceeec0061e5c9d252
Rob Simmonds <simmonds@cs.uct.ac.za>
2019-05-26 14:17:11 +0200

    Fixed previous update...

--- OnMessageTask.cc
+++ OnMessageTask.cc
@@ -96,8 +96,8 @@ tbb::task* SetImageChannelsTask::execute() {
     _session->ImageChannelUnlock();
 
     if (tester) {
-        tbb::increment_ref_count();
-        tbb::recycle_as_safe_continuation();
+        increment_ref_count();
+        recycle_as_safe_continuation();
     }
 
     return nullptr;
@@ -124,8 +124,8 @@ tbb::task* SetHistogramRequirementsTask::execute() {
 
 tbb::task* AnimationTask::execute() {
     if (_session->ExecuteAnimationFrame()) {
-        tbb::increment_ref_count();
-        tbb::recycle_as_safe_continuation();
+        increment_ref_count();
+        recycle_as_safe_continuation();
     } else {
         if (_session->waitingFlowEvent()) {
             _session->setWaitingTask_ptr(this);
