9f532e307ba713f791f8dcc6ecfcd1787bdfb75e
Rob Simmonds <simmonds@cs.uct.ac.za>
2019-06-10 15:56:12 +0200

    Fixed formatting

--- FileInfoLoader.h
+++ FileInfoLoader.h
@@ -7,8 +7,8 @@
 
 #include <casacore/casa/Arrays/IPosition.h>
 #include <casacore/fits/FITS/FITSError.h>
-#include <casacore/fits/FITS/hdu.h> // hdu.h must come before fitsio.h
 #include <casacore/fits/FITS/fitsio.h>
+#include <casacore/fits/FITS/hdu.h> // hdu.h must come before fitsio.h
 #include <casacore/images/Images/ImageOpener.h>
 #include <casacore/measures/Measures/MDirection.h>
 #include <casacore/measures/Measures/MFrequency.h>
