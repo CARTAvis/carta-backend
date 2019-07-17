64d20d40a57c47602e92e48d029580064b1e6f74
Mark C.C. Chiang <chcchiang@asiaa.sinica.edu.tw>
2019-07-17 23:23:17 +0800

    add a max channel setting

--- Region/Region.cc
+++ Region/Region.cc
@@ -416,6 +416,9 @@ bool Region::MakeExtensionBox(casacore::WCBox& extend_box, int stokes, ChannelRa
     try {
         double min_chan(channel_range.from), max_chan(channel_range.to);
         double all_channels = _image_shape(_spectral_axis);
+        if (channel_range.to == ALL_CHANNELS) { // extend to nchan
+            max_chan = all_channels - 1;
+        }
         assert((max_chan >= min_chan) && (all_channels > max_chan));
         if (max_chan < 0 || min_chan < 0) {
             std::cerr << "ERROR: max(" << max_chan << ") or min(" << min_chan << ") channel is negative!" << std::endl;
