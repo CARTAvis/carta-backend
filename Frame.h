b576c7e831e57796890777636403b9a04715f5f7
Mark C.C. Chiang <chcchiang@asiaa.sinica.edu.tw>
2019-08-06 17:02:39 +0800

    Add current region id as an interruption condition

--- Frame.h
+++ Frame.h
@@ -105,7 +105,7 @@ public:
     RegionState GetRegionState(int region_id);
 
     // Interrupt conditions
-    bool Interrupt(const CursorXy& cursor1, const CursorXy& cursor2); // cursor and point regions
+    bool Interrupt(int region_id, const CursorXy& cursor1, const CursorXy& cursor2); // cursor and point regions
     bool Interrupt(int region_id, const RegionState& region_state);
     bool Interrupt(int region_id, const RegionState& region_state, const ZProfileWidget& config_stats);
 
@@ -158,9 +158,11 @@ private:
     // Functions used to set cursor and region states
     void SetConnectionFlag(bool connected);
     void SetCursorXy(float x, float y);
+    void SetRegionId(int region_id);
 
     // Functions used to check cursor and region states
     bool IsConnected();
+    bool IsSameRegionId(int region_id);
     bool IsSameRegionState(int region_id, const RegionState& region_state);
     bool IsSameRegionSpectralConfig(int region_id, const ZProfileWidget& config_stats);
 
@@ -199,6 +201,8 @@ private:
     volatile bool _connected = true;
     // Current cursor's x-y coordinate
     CursorXy _cursor_xy;
+    // Current region id;
+    int _region_id;
 };
 
 #endif // CARTA_BACKEND__FRAME_H_
