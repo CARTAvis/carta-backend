15ca8bc963a0894456295809d95ff6002ef7fa8e
Pam Ford <pford@nrao.edu>
2019-08-08 11:35:03 -0600

    Add casacore image header

--- Region/RegionStats.h
+++ Region/RegionStats.h
@@ -6,6 +6,8 @@
 #include <unordered_map>
 #include <vector>
 
+#include <casacore/images/Images/ImageInterface.h>
+
 #include <carta-protobuf/defs.pb.h>                // Histogram, StatisticsValue
 #include <carta-protobuf/region_requirements.pb.h> // HistogramConfig
 #include <carta-protobuf/region_stats.pb.h>        // RegionStatsData
