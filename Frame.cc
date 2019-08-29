f57ecaf0976d336002892dfd2e795bcf6f369629
Cheng-Chin Chiang <chcchiang@asiaa.sinica.edu.tw>
2019-08-29 22:25:30 +0800

    Add region spectral profile cache for non-HDF5 images (#333)
    * All stats calculated for a spectral profile are stored in a Region map

--- Frame.cc
+++ Frame.cc
@@ -1592,17 +1592,20 @@ bool Frame::GetRegionSpectralData(int region_id, int config_stokes, int profile_
     bool data_ok(false);
     if (_regions.count(region_id)) {
         auto& region = _regions[region_id];
-
-        // initialize the spectral data
         std::map<CARTA::StatsType, std::vector<double>> results;
+        size_t start;
         size_t profile_size = NumChannels(); // total number of channels
-        if (!region->InitSpectralData(config_stokes, profile_size, results)) {
-            // config removed or no unsent stats
-            return false;
+
+        // Try to get the cache or initialize spectral profiles to NaN
+        region->InitSpectralData(profile_stokes, profile_size, results, start);
+
+        // If stats cache is available and completed, don't need to recalculate spectral profiles.
+        if (start == profile_size) {
+            partial_results_callback(results, 1.0f);
+            return true;
         }
 
-        // initialize the variables
-        size_t start = 0;
+        // Initialize variables and calculate spectral profiles
         size_t count, end;
         float progress = 0;
         casacore::SubImage<float> sub_image;
@@ -1617,6 +1620,10 @@ bool Frame::GetRegionSpectralData(int region_id, int config_stokes, int profile_
 
             // check if region, current stokes, spectral requirements changed
             if (Interrupt(region_id, profile_stokes, start_region_state, start_config_stats)) {
+                // Save stats cache if region state is not changed
+                if (IsSameRegionState(region_id, start_region_state)) {
+                    region->SetStatsCache(profile_stokes, results, start);
+                }
                 return data_ok; // false until complete
             }
 
@@ -1630,7 +1637,7 @@ bool Frame::GetRegionSpectralData(int region_id, int config_stokes, int profile_
             if (has_subimage) {
                 std::map<CARTA::StatsType, std::vector<double>> buffers;
                 std::unique_lock<std::mutex> ulock2(_image_mutex);
-                bool has_data = region->GetSpectralProfileData(buffers, config_stokes, sub_image);
+                bool has_data = region->GetSpectralProfileData(buffers, sub_image);
                 ulock2.unlock();
                 if (has_data) {
                     for (const auto& buffer : buffers) {
@@ -1670,6 +1677,11 @@ bool Frame::GetRegionSpectralData(int region_id, int config_stokes, int profile_
             if (dt_partial_profile > TARGET_PARTIAL_REGION_TIME || progress >= 1.0f) {
                 t_partial_profile_start = std::chrono::high_resolution_clock::now();
                 partial_results_callback(results, progress);
+            }
+
+            // Save stats cache while spectral profiles are completed and region state is not changed
+            if (progress == 1.0f && IsSameRegionState(region_id, start_region_state)) {
+                region->SetStatsCache(profile_stokes, results, start);
             }
         }
         data_ok = true;
