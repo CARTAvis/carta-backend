ae32c032e89261292eb6185c68ae34d08cc0ca9a
veggiesaurus <accomrie@gmail.com>
2019-11-28 15:22:23 +0200

    added basic mean and sigma calculation to min/max calculation when calculating histograms

--- Region/MinMax.h
+++ Region/MinMax.h
@@ -14,6 +14,8 @@ namespace carta {
 template <typename T>
 class MinMax {
     T _min_val, _max_val;
+    double _sum, _sum_squares;
+    size_t _value_counter;
     const std::vector<T>& _data;
 
 public:
@@ -24,15 +26,17 @@ public:
     void join(MinMax& other); // NOLINT
 
     std::pair<T, T> GetMinMax() const;
+    double GetMean() const;
+    double GetSigma() const;
 };
 
 template <typename T>
 MinMax<T>::MinMax(const std::vector<T>& data)
-    : _min_val(std::numeric_limits<T>::max()), _max_val(std::numeric_limits<T>::lowest()), _data(data) {}
+    : _min_val(std::numeric_limits<T>::max()), _max_val(std::numeric_limits<T>::lowest()), _sum(0), _sum_squares(0), _value_counter(0), _data(data) {}
 
 template <typename T>
 MinMax<T>::MinMax(MinMax<T>& mm, tbb::split)
-    : _min_val(std::numeric_limits<T>::max()), _max_val(std::numeric_limits<T>::lowest()), _data(mm._data) {}
+    : _min_val(std::numeric_limits<T>::max()), _max_val(std::numeric_limits<T>::lowest()), _sum(0), _sum_squares(0), _value_counter(0), _data(mm._data) {}
 
 template <typename T>
 void MinMax<T>::operator()(const tbb::blocked_range<size_t>& r) {
@@ -47,6 +51,9 @@ void MinMax<T>::operator()(const tbb::blocked_range<size_t>& r) {
             if (val > t_max) {
                 t_max = val;
             }
+            _value_counter++;
+            _sum += val;
+            _sum_squares += val * val;
         }
     }
     _min_val = t_min;
@@ -57,12 +64,33 @@ template <typename T>
 void MinMax<T>::join(MinMax<T>& other) { // NOLINT
     _min_val = std::min(_min_val, other._min_val);
     _max_val = std::max(_max_val, other._max_val);
+    _value_counter += other._value_counter;
+    _sum += other._sum;
+    _sum_squares += other._sum_squares;
 }
 
 template <typename T>
 std::pair<T, T> MinMax<T>::GetMinMax() const {
     return {_min_val, _max_val};
 }
+
+template <typename T>
+double MinMax<T>::GetMean() const {
+    if (_value_counter > 0) {
+        return _sum / _value_counter;
+    }
+    return NAN;
+}
+
+template <typename T>
+double MinMax<T>::GetSigma() const {
+    if (_value_counter > 0) {
+        double mean = GetMean();
+        return sqrt(_sum_squares / _value_counter - mean * mean);
+    }
+    return NAN;
+}
+
 
 } // namespace carta
 
