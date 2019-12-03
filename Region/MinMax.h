dad8151fb494d9dcc79171c96e6ce808b01dd295
veggiesaurus <accomrie@gmail.com>
2019-12-03 09:58:19 +0200

    adjusted naming of basic stats values in MinMax

--- Region/MinMax.h
+++ Region/MinMax.h
@@ -15,7 +15,7 @@ template <typename T>
 class MinMax {
     T _min_val, _max_val;
     double _sum, _sum_squares;
-    size_t _value_counter;
+    size_t _num_pixels;
     const std::vector<T>& _data;
 
 public:
@@ -27,16 +27,17 @@ public:
 
     std::pair<T, T> GetMinMax() const;
     double GetMean() const;
-    double GetSigma() const;
+    double GetStdDev() const;
+    size_t GetNumPixels() const;
 };
 
 template <typename T>
 MinMax<T>::MinMax(const std::vector<T>& data)
-    : _min_val(std::numeric_limits<T>::max()), _max_val(std::numeric_limits<T>::lowest()), _sum(0), _sum_squares(0), _value_counter(0), _data(data) {}
+    : _min_val(std::numeric_limits<T>::max()), _max_val(std::numeric_limits<T>::lowest()), _sum(0), _sum_squares(0), _num_pixels(0), _data(data) {}
 
 template <typename T>
 MinMax<T>::MinMax(MinMax<T>& mm, tbb::split)
-    : _min_val(std::numeric_limits<T>::max()), _max_val(std::numeric_limits<T>::lowest()), _sum(0), _sum_squares(0), _value_counter(0), _data(mm._data) {}
+    : _min_val(std::numeric_limits<T>::max()), _max_val(std::numeric_limits<T>::lowest()), _sum(0), _sum_squares(0), _num_pixels(0), _data(mm._data) {}
 
 template <typename T>
 void MinMax<T>::operator()(const tbb::blocked_range<size_t>& r) {
@@ -51,7 +52,7 @@ void MinMax<T>::operator()(const tbb::blocked_range<size_t>& r) {
             if (val > t_max) {
                 t_max = val;
             }
-            _value_counter++;
+            _num_pixels++;
             _sum += val;
             _sum_squares += val * val;
         }
@@ -64,7 +65,7 @@ template <typename T>
 void MinMax<T>::join(MinMax<T>& other) { // NOLINT
     _min_val = std::min(_min_val, other._min_val);
     _max_val = std::max(_max_val, other._max_val);
-    _value_counter += other._value_counter;
+    _num_pixels += other._num_pixels;
     _sum += other._sum;
     _sum_squares += other._sum_squares;
 }
@@ -76,19 +77,24 @@ std::pair<T, T> MinMax<T>::GetMinMax() const {
 
 template <typename T>
 double MinMax<T>::GetMean() const {
-    if (_value_counter > 0) {
-        return _sum / _value_counter;
+    if (_num_pixels > 0) {
+        return _sum / _num_pixels;
     }
     return NAN;
 }
 
 template <typename T>
-double MinMax<T>::GetSigma() const {
-    if (_value_counter > 0) {
+double MinMax<T>::GetStdDev() const {
+    if (_num_pixels > 0) {
         double mean = GetMean();
-        return sqrt(_sum_squares / _value_counter - mean * mean);
+        return sqrt(_sum_squares / _num_pixels - mean * mean);
     }
     return NAN;
+}
+
+template <typename T>
+size_t MinMax<T>::GetNumPixels() const {
+    return _num_pixels;
 }
 
 
