3b31f78cbd37fb2e048008e1ba3ad9a0ebbdf025
Cheng-Chin Chiang <chcchiang@asiaa.sinica.edu.tw>
2020-11-02 20:16:54 +0800

    Refactor the moment generator (#634)
    * Add a mutex for moments calculation
    
    * Catch an error message if the error of WCBox conversion happens
    
    * Reset shared pointers for output moments images if the calculation is cancelled
    
    * Add a lock for spectral profile or image moments calculations
    
    * Stop the moment calculation while the spectral profile calculation begins
    
    * Stop the old moments calculation while a new one begins
    
    * Erase a moment generator while closing a source image
    
    * Add a bool variable for the moment generator cancellation
    
    * Check whether to report the progress between slices calculations
    
    * Simplify the way to get the full directional image region
    
    * Remove the MomentController and move the MomentGenerator under the Frame
    
    * Fix the code style
    
    * Refactor the MomentGenerator
    
    * Modify the style of moments calculation function
    
    * Modify the style of moments calculation function
    
    * Correct the protobuf commit version
    
    * Set an alias for carta::RegionHandler namespace
    
    * Initialize the moment generator in the Frame construction
    
    * Refactor the moment calculation function
    
    * Remove the alias for carta::RegionHandler namespace
    
    * Remove the FileConverter and move the saving file function under the Frame
    
    * Construct the MomentGenerator when moments are requested
    
    * Further simplify the GetImageRegion function
    
    * Move the logic of calculating moments with a region_id to the Session
    
    * Add a moments count variable to control the destruction of Frame
    
    * Use slash operator to concatenate the file path and name
    
    * Use slash operator to concatenate the file path and name
    
    * Convert the file path name to the string type for comparison

--- Region/RegionHandler.h
+++ Region/RegionHandler.h
@@ -65,11 +65,10 @@ public:
         std::function<void(CARTA::SpectralProfileData profile_data)> cb, int region_id, int file_id, bool stokes_changed);
     bool FillRegionStatsData(std::function<void(CARTA::RegionStatsData stats_data)> cb, int region_id, int file_id);
 
-    // Get an image region with respect to the region id, file id, channels range and a stoke type
-    bool ApplyRegionToFile(int region_id, int file_id, const ChannelRange& channel, int stokes, casacore::ImageRegion& region);
-
-    // Get file ids with respect to the region id
-    std::set<int> GetFileIds(int region_id);
+    // Calculate moments
+    bool CalculateMoments(int file_id, int region_id, const std::shared_ptr<Frame>& frame, MomentProgressCallback progress_callback,
+        const CARTA::MomentRequest& moment_request, CARTA::MomentResponse& moment_response,
+        std::vector<carta::CollapseResult>& collapse_results);
 
 private:
     // Get unique region id (max id + 1)
@@ -96,6 +95,7 @@ private:
     // Fill data stream messages
     bool RegionFileIdsValid(int region_id, int file_id);
     casacore::LCRegion* ApplyRegionToFile(int region_id, int file_id);
+    bool ApplyRegionToFile(int region_id, int file_id, const ChannelRange& channel, int stokes, casacore::ImageRegion& region);
     bool GetRegionHistogramData(
         int region_id, int file_id, std::vector<HistogramConfig>& configs, CARTA::RegionHistogramData& histogram_message);
     bool GetRegionSpectralData(int region_id, int file_id, std::string& coordinate, int stokes_index,
