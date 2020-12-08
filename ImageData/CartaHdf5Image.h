666a2206e4bbb3229d9b5857928347dadd33209f
Adrianna Pińska <adrianna.pinska@gmail.com>
2020-12-08 10:41:26 +0200

    Script for checking and adding copyright and licence headers, and first pass to add them (#668)
    * header script -- untested!
    
    * Switched to multiline comment style; tested; seems to work
    
    * initial pass to add headers
    
    * Adding new script to Circle CI config file
    
    * Checking if the Circle CI test will fail
    
    * Check complete. Putting Session.cc back to the way it was.
    
    * Check complete. Putting Session.cc back to the way it was.
    
    * tweak to format check config
    
    Co-authored-by: ajm-asiaa <ajm@asiaa.sinica.edu.tw>

--- ImageData/CartaHdf5Image.h
+++ ImageData/CartaHdf5Image.h
@@ -1,3 +1,9 @@
+/* This file is part of the CARTA Image Viewer: https://github.com/CARTAvis/carta-backend
+   Copyright 2018, 2019, 2020 Academia Sinica Institute of Astronomy and Astrophysics (ASIAA),
+   Associated Universities, Inc. (AUI) and the Inter-University Institute for Data Intensive Astronomy (IDIA)
+   SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
 //# CartaHdf5Image.h : HDF5 Image class derived from casacore::ImageInterface
 
 #ifndef CARTA_BACKEND_IMAGEDATA_CARTAHDF5IMAGE_H_
