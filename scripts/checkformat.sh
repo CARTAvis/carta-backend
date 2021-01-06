37ff58059f75116e8c617413eb61b2b633eed696
Pam Harris <pharris@nrao.edu>
2021-01-06 14:50:47 -0700

    Update checkformat script exclude uWebSockets directory

--- scripts/checkformat.sh
+++ scripts/checkformat.sh
@@ -7,7 +7,7 @@
 # If changes are necessary the script will print a diff -- please run reformat.sh to fix the code.
 # The clang-format configuration is found in .clang-format
 
-PARAMS=(\( -regex '.*\.\(cc\|h\)' -not -path './*/carta-protobuf/*' -not -path './*/carta-scripting-grpc/*' \))
+PARAMS=(\( -regex '.*\.\(cc\|h\)' -not -path './*/carta-protobuf/*' -not -path './*/carta-scripting-grpc/*' -not -path './uWebSockets/*/*' \))
 
 find . "${PARAMS[@]}" -exec cat {} \; | diff -u <(find . "${PARAMS[@]}" -exec clang-format {} \;) -
 
