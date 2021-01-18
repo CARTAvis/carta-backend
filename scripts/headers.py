5d7dfb927b1e050622e501f545f6a70f83839f6f
Adrianna Pińska <adrianna.pinska@gmail.com>
2021-01-18 15:20:32 +0200

    bump copyright years

--- scripts/headers.py
+++ scripts/headers.py
@@ -6,7 +6,7 @@ import re
 import argparse
 
 HEADER = """/* This file is part of the CARTA Image Viewer: https://github.com/CARTAvis/carta-backend
-   Copyright 2018, 2019, 2020 Academia Sinica Institute of Astronomy and Astrophysics (ASIAA),
+   Copyright 2018, 2019, 2020, 2021 Academia Sinica Institute of Astronomy and Astrophysics (ASIAA),
    Associated Universities, Inc. (AUI) and the Inter-University Institute for Data Intensive Astronomy (IDIA)
    SPDX-License-Identifier: GPL-3.0-or-later
 */
