8ba3beff65b6495a1eae10a04e9b9857627496f9
Jorge Lopez <jorgext@gmail.com>
2021-02-12 12:55:45 -0300

    restore histogram test from dev

--- test/TestHistogram.cc
+++ test/TestHistogram.cc
@@ -4,7 +4,6 @@
    SPDX-License-Identifier: GPL-3.0-or-later
 */
 
-#include <cmath> // for NAN
 #include <random>
 #include <vector>
 
@@ -16,118 +15,35 @@
 #include "Timer/Timer.h"
 
 using namespace std;
-random_device rd;
-mt19937 mt(rd());
-uniform_real_distribution<float> float_random(0, 1.0f);
 
-bool CompareResults(const carta::HistogramResults& a, const carta::HistogramResults& b) {
-    if (a.num_bins != b.num_bins || a.bin_center != b.bin_center || a.bin_width != b.bin_width) {
-        return false;
+class HistogramTest : public ::testing::Test {
+public:
+    random_device rd;
+    mt19937 mt;
+    uniform_real_distribution<float> float_random;
+
+    HistogramTest() {
+        mt = mt19937(rd());
+        float_random = uniform_real_distribution<float>(0, 1.0f);
     }
-
-    for (auto i = 0; i < a.num_bins; i++) {
-        auto bin_a = a.histogram_bins[i];
-        auto bin_b = b.histogram_bins[i];
-        if (bin_a != bin_b) {
+    static bool CompareResults(const carta::HistogramResults& a, const carta::HistogramResults& b) {
+        if (a.num_bins != b.num_bins || a.bin_center != b.bin_center || a.bin_width != b.bin_width) {
             return false;
         }
+
+        for (auto i = 0; i < a.num_bins; i++) {
+            auto bin_a = a.histogram_bins[i];
+            auto bin_b = b.histogram_bins[i];
+            if (bin_a != bin_b) {
+                return false;
+            }
+        }
+
+        return true;
     }
+};
 
-    return true;
-}
-
-carta::Histogram CalcHistogram(int nbins, float min, float max, const std::vector<float>& data) {
-    if (false) {
-        return carta::Histogram(1, 0, 0, {});
-    } else {
-        return carta::Histogram(nbins, min, max, data);
-    }
-}
-
-TEST(Histogram, TestHistogramUsage) {
-    std::vector<float> data(1024 * 1024);
-    std::for_each(data.begin(), data.end(), [](float& v) { v = float_random(mt); });
-    carta::Histogram hist = CalcHistogram(1024, 0, 1, data);
-    EXPECT_TRUE(hist.GetNbins() == 1024) << "Wrong number of bins";
-    EXPECT_TRUE(hist.GetMinVal() == 0) << "Wrong min value";
-    EXPECT_TRUE(hist.GetMaxVal() == 1) << "Wrong max value";
-}
-
-TEST(Histogram, TestHistogramBehaviour) {
-    std::vector<float> data;
-    // test histogram filling
-    data.push_back(0.0);  // should go to bin at pos. 0 - first bin is closed from below
-    data.push_back(0.5);  // should go to bin at pos. 0
-    data.push_back(1.0);  // should go to bin at pos. 1 - middle bins are semi-open, just closed from below
-    data.push_back(4.0);  // should go to bin at pos. 4
-    data.push_back(4.5);  // should go to bin at pos. 4
-    data.push_back(4.7);  // should go to bin at pos. 4
-    data.push_back(4.9);  // should go to bin at pos. 4
-    data.push_back(5.0);  // should go to bin at pos. 5
-    data.push_back(5.0);  // should go to bin at pos. 5
-    data.push_back(5.0);  // should go to bin at pos. 5
-    data.push_back(9.1);  // should go to bin at pos. 9
-    data.push_back(10.0); // should go to bin at pos. 9 - last bin is closed from above
-    // values that will fall in the overflow and underflow range
-    data.push_back(-1.0);        // should not appear
-    data.push_back(00.0 - 1e-9); // should not appear
-    data.push_back(10.0 + 1e+9); // should not appear
-    data.push_back(11.0);        // should not appear
-    carta::Histogram hist(10, 0.0f, 10.0f, data);
-    auto results = hist.GetHistogram();
-    auto counts = accumulate(results.histogram_bins.begin(), results.histogram_bins.end(), 0);
-    EXPECT_TRUE(counts == 12);
-    EXPECT_TRUE(results.histogram_bins[0] == 2);
-    EXPECT_TRUE(results.histogram_bins[1] == 1);
-    EXPECT_TRUE(results.histogram_bins[4] == 4);
-    EXPECT_TRUE(results.histogram_bins[5] == 3);
-    EXPECT_TRUE(results.histogram_bins[9] == 2);
-
-    data.clear();
-    data.push_back(NAN);
-    data.push_back(NAN);
-    data.push_back(NAN);
-    carta::Histogram hist2(10, 0.0f, 10.0f, data);
-    // expect 0 counts
-    auto bins = hist2.GetHistogramBins();
-    EXPECT_TRUE(accumulate(bins.begin(), bins.end(), 0) == 0);
-}
-
-TEST(Histogram, TestHistogramConstructor) {
-    std::vector<float> data(1024 * 1024);
-    std::for_each(data.begin(), data.end(), [](float& v) { v = float_random(mt); });
-    carta::Histogram hist(1024, 0.0f, 1.0f, data);
-    auto results = hist.GetHistogram();
-
-    carta::Histogram hist2(hist);
-    auto results2 = hist2.GetHistogram();
-
-    EXPECT_TRUE(CompareResults(results, results2));
-}
-
-TEST(Histogram, TestHistogramJoin) {
-    std::vector<float> data(1024 * 1024);
-    std::for_each(data.begin(), data.end(), [](float& v) { v = float_random(mt); });
-    carta::Histogram hist(1024, 0.0f, 1.0f, data);
-    auto results = hist.GetHistogram();
-    const auto total_counts = accumulate(results.histogram_bins.begin(), results.histogram_bins.end(), 0);
-
-    carta::Histogram hist2(1024, 0.0f, 1.0f, data);
-    auto results2 = hist2.GetHistogram();
-
-    EXPECT_TRUE(CompareResults(results, results2)); // naive?
-
-    hist.join(hist2);
-    results = hist.GetHistogram();
-    const auto total_counts2 = accumulate(results.histogram_bins.begin(), results.histogram_bins.end(), 0);
-
-    EXPECT_TRUE(total_counts * 2 == total_counts2);
-
-    carta::Histogram hist3(512, 0.0f, 1.0f, data);
-    EXPECT_FALSE(hist.join(hist3));
-}
-
-TEST(Histogram, TestSingleThreading) {
+TEST_F(HistogramTest, TestSingleThreading) {
     std::vector<float> data(1024 * 1024);
     for (auto& v : data) {
         v = float_random(mt);
@@ -135,16 +51,18 @@ TEST(Histogram, TestSingleThreading) {
 
     carta::ThreadManager::SetThreadLimit(1);
     carta::Histogram hist_st(1024, 0.0f, 1.0f, data);
+    hist_st.setup_bins();
     auto results_st = hist_st.GetHistogram();
 
     for (auto i = 2; i < 24; i++) {
-        carta::Histogram hist_mt(1024, 0.0f, 1.0f, data);
-        auto results_mt = hist_mt.GetHistogram();
+        carta::Histogram mt(1024, 0.0f, 1.0f, data);
+        mt.setup_bins();
+        auto results_mt = mt.GetHistogram();
         EXPECT_TRUE(CompareResults(results_st, results_mt));
     }
 }
 
-TEST(Histogram, TestMultithreading) {
+TEST_F(HistogramTest, TestMultithreading) {
     std::vector<float> data(1024 * 1024);
     for (auto& v : data) {
         v = float_random(mt);
@@ -152,17 +70,20 @@ TEST(Histogram, TestMultithreading) {
 
     carta::ThreadManager::SetThreadLimit(1);
     carta::Histogram hist_st(1024, 0.0f, 1.0f, data);
+    hist_st.setup_bins();
     auto results_st = hist_st.GetHistogram();
 
     for (auto i = 2; i < 24; i++) {
         carta::ThreadManager::SetThreadLimit(i);
-        carta::Histogram hist_mt(1024, 0.0f, 1.0f, data);
-        auto results_mt = hist_mt.GetHistogram();
+        carta::Histogram mt(1024, 0.0f, 1.0f, data);
+        mt.setup_bins();
+        auto results_mt = mt.GetHistogram();
         EXPECT_TRUE(CompareResults(results_st, results_mt));
     }
 }
+#ifdef NDEBUG
 
-TEST(Histogram, TestMultithreadingPerformance) {
+TEST_F(HistogramTest, TestMultithreadingPerformance) {
     std::vector<float> data(1024 * 1024);
     for (auto& v : data) {
         v = float_random(mt);
@@ -173,17 +94,22 @@ TEST(Histogram, TestMultithreadingPerformance) {
 
     t.Start("single_threaded");
     carta::Histogram hist_st(1024, 0.0f, 1.0f, data);
+    hist_st.setup_bins();
     auto results_st = hist_st.GetHistogram();
     t.End("single_threaded");
 
     carta::ThreadManager::SetThreadLimit(4);
     t.Start("multi_threaded");
-    carta::Histogram hist_mt(1024, 0.0f, 1.0f, data);
-    auto results_mt = hist_mt.GetHistogram();
+    carta::Histogram mt(1024, 0.0f, 1.0f, data);
+    mt.setup_bins();
+    auto results_mt = mt.GetHistogram();
     t.End("multi_threaded");
 
     auto st_time = t.GetMeasurement("single_threaded");
     auto mt_time = t.GetMeasurement("multi_threaded");
     double speedup = st_time / mt_time;
-    EXPECT_GE(speedup, 1.5) << "Speedup is: " << speedup;
+    cout << speedup << endl;
+    EXPECT_GE(speedup, 1.5);
 }
+
+#endif
