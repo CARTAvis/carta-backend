2bdeca78fc06b26a4792e9247b64781e19a4b7af
Mark C.C. Chiang <chcchiang@asiaa.sinica.edu.tw>
2021-02-25 16:37:18 +0800

    remove the square brackets

--- src/Logger/Logger.cc
+++ src/Logger/Logger.cc
@@ -89,7 +89,7 @@ void LogReceivedEventType(const CARTA::EventType& event_type) {
     auto event_name = CARTA::EventType_Name(CARTA::EventType(event_type));
     if (!event_name.empty()) {
         if (log_icd_messages) {
-            spdlog::debug("[<==] {}", event_name);
+            spdlog::debug("<== {}", event_name);
         }
     } else {
         spdlog::error("Unknown event type: {}!", event_type);
@@ -100,7 +100,7 @@ void LogSentEventType(const CARTA::EventType& event_type) {
     auto event_name = CARTA::EventType_Name(CARTA::EventType(event_type));
     if (!event_name.empty()) {
         if (log_icd_messages) {
-            spdlog::debug("[==>] {}", event_name);
+            spdlog::debug("==> {}", event_name);
         }
     } else {
         spdlog::error("Unknown event type: {}!", event_type);
