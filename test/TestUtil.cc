4b8b4773560b3ea832881fbebfbc79f5d70349d7
Adrianna Pińska <adrianna.pinska@gmail.com>
2021-05-24 17:11:54 +0200

    formatting pass

--- test/TestUtil.cc
+++ test/TestUtil.cc
@@ -61,10 +61,10 @@ TEST(UtilTest, ParentNotSubdirectory) {
     auto pwd = TestRoot();
     // Temporary change to debug failing test on Mac
     EXPECT_EQ(pwd, "dummy");
-    
-//     EXPECT_FALSE(IsSubdirectory(pwd.parent_path().string(), pwd.string()));
-//     EXPECT_FALSE(IsSubdirectory((pwd / "..").string(), pwd.string()));
-//     EXPECT_FALSE(IsSubdirectory("../", "./"));
+
+    //     EXPECT_FALSE(IsSubdirectory(pwd.parent_path().string(), pwd.string()));
+    //     EXPECT_FALSE(IsSubdirectory((pwd / "..").string(), pwd.string()));
+    //     EXPECT_FALSE(IsSubdirectory("../", "./"));
 }
 
 TEST(UtilTest, TopIsRoot) {
