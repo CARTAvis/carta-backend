aed1f8744674e498e7c46600b3de8d85aeb5a82d
Adrianna Pińska <adrianna.pinska@gmail.com>
2021-06-17 08:33:48 +0200

    Add basic tests for spatial profiles (#841)
    * most of a basic test
    
    * Added profile vector comparisons
    
    * Added basic FITS data reader to provide expected values for tests.
    
    * Renamed functions and changed indexing to be more intuitive
    
    * Implemented HDF5 data reader.
    
    * moved environment variable change to setup
    
    * formatting pass
    
    * Simplified default destructor
    
    * change NULL to nullptr

--- test/CommonTestUtilities.h
+++ test/CommonTestUtilities.h
@@ -9,6 +9,8 @@
 
 #include <unordered_set>
 
+#include <H5Cpp.h>
+#include <fitsio.h>
 #include <gtest/gtest.h>
 #include <spdlog/fmt/fmt.h>
 
@@ -40,6 +42,40 @@ public:
     static std::string Hdf5ImagePath(const std::string& filename);
     static std::string FitsTablePath(const std::string& filename);
     static std::string XmlTablePath(const std::string& filename);
+};
+
+class DataReader {
+public:
+    virtual std::vector<float> ReadRegion(std::vector<hsize_t> start, std::vector<hsize_t> end) = 0;
+    float ReadPointXY(hsize_t x, hsize_t y, hsize_t channel = 0, hsize_t stokes = 0);
+    std::vector<float> ReadProfileX(hsize_t y, hsize_t channel = 0, hsize_t stokes = 0);
+    std::vector<float> ReadProfileY(hsize_t x, hsize_t channel = 0, hsize_t stokes = 0);
+
+protected:
+    int _N;
+    std::vector<hsize_t> _dims;
+    hsize_t _stokes, _depth, _height, _width;
+};
+
+class FitsDataReader : public DataReader {
+public:
+    FitsDataReader(const std::string& imgpath);
+    ~FitsDataReader();
+    std::vector<float> ReadRegion(std::vector<hsize_t> start, std::vector<hsize_t> end) override;
+
+private:
+    fitsfile* _imgfile;
+};
+
+class Hdf5DataReader : public DataReader {
+public:
+    Hdf5DataReader(const std::string& imgpath);
+    ~Hdf5DataReader() = default;
+    std::vector<float> ReadRegion(std::vector<hsize_t> start, std::vector<hsize_t> end) override;
+
+private:
+    H5::H5File _imgfile;
+    H5::DataSet _dataset;
 };
 
 class CartaEnvironment : public ::testing::Environment {
