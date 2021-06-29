01442b2381e6277bbea82567afb882d47092033d
Adrianna Pińska <adrianna.pinska@gmail.com>
2021-06-29 20:39:57 +0200

    Migrated format check/fix and not yet usable style check into new script and removed old scripts

--- scripts/style.py
+++ scripts/style.py
@@ -4,9 +4,13 @@ import sys
 import os
 import re
 import argparse
+import subprocess
 
 class Test:
     TESTS = {}
+    PYTHON_FILE_REGEX = r".*\.(cc|h|tcc)$"
+    SHELL_FILE_REGEX = r".*\.\(cc\|h\|tcc\)$"
+    EXCLUDE_FROM_ALL = False
     
     def __init_subclass__(cls, **kwargs):
         name = cls.__name__.lower()
@@ -16,7 +20,7 @@ class Test:
     @classmethod
     def get_tests(cls, testname):
         if testname == "all":
-            return cls.TESTS.values()
+            return tuple(test for test in cls.TESTS.values() if not test.EXCLUDE_FROM_ALL)
         return (cls.TESTS[testname],)
     
     @staticmethod
@@ -24,7 +28,7 @@ class Test:
         for root, dirs, files in os.walk(directory):
             dirs[:] = [d for d in dirs]
             for basename in files:
-                if re.search("\.(cc|h|tcc)$", basename):
+                if re.match(Test.PYTHON_FILE_REGEX, basename):
                     filename = os.path.join(root, basename)
                     yield filename
     
@@ -98,32 +102,71 @@ class Header(Test):
                         f.write(Header.HEADER)
                         f.write("\n")
                         f.write(data)
+        
+        return 0
 
 class Format(Test):
     @staticmethod
-    def check(directories, quiet):
-        raise NotImplementedError()
+    def check(directories, quiet):        
+        find_files = fr"find {' '.join(directories)} -regex '{Test.SHELL_FILE_REGEX}'"
+        process = subprocess.run(fr"{find_files} -exec clang-format {{}} \; | diff -u <({find_files} -exec cat {{}} \;) -", shell=True, executable="/bin/bash", capture_output=True)
+        
+        if process.stdout and not quiet:
+            print(process.stdout.decode())
+        
+        if process.stderr:
+            print(process.stderr.decode())
+        
+        return process.returncode
     
     @staticmethod
     def fix(directories, quiet):
-        raise NotImplementedError()
-
+        find_files = fr"find {' '.join(directories)} -regex '{Test.SHELL_FILE_REGEX}'"
+        process = subprocess.run(fr"{find_files} | xargs clang-format -i", shell=True, executable="/bin/bash", capture_output=True)
+                
+        if process.stdout and not quiet:
+            print(process.stdout.decode())
+        
+        if process.stderr:
+            print(process.stderr.decode())
+        
+        return process.returncode
+        
 class Style(Test):
+    EXCLUDE_FROM_ALL = True
+    
     @staticmethod
     def check(directories, quiet):
-        raise NotImplementedError()
+        status = 0
+        
+        tidy_regex = f"\"$PWD/({'|'.join(directories)})/.*\""
+        process = subprocess.run(fr"run-clang-tidy -quiet -extra-arg-before=-fno-caret-diagnostics -p build -header-filter={tidy_regex} {tidy_regex}", shell=True, executable="/bin/bash", capture_output=True)
+                
+        if process.stdout and not quiet:
+            print(process.stdout.decode())
+        
+        if process.stderr:
+            print(process.stderr.decode())
+        
+        status = process.returncode | bool(process.stdout)
+        
+        return status
     
     @staticmethod
     def fix(directories, quiet):
-        raise NotImplementedError()
+        if not quiet:
+            print("Automatic style fixes are not yet implemented.")
+        return 1
 
 if __name__ == "__main__":    
     parser = argparse.ArgumentParser(description="Check or fix code format, style, or copyright and licence headers.")
-    parser.add_argument('test', help="Test to perform (header, format, style or all)", choices=(*Test.TESTS.keys(), "all"))
+    parser.add_argument('test', help="Test to perform (header, format, style or all; style is currently excluded from all).", choices=(*Test.TESTS.keys(), "all"))
     parser.add_argument('command', help="Command (check or fix).", choices=("check", "fix"))
     parser.add_argument('-d', '--directory', help="Location of the root directory; can be used multiple times. Defaults to the `src' and `test' subdirectories in the current directory.", default=[], action="append")
     parser.add_argument('-q', '--quiet', help="Suppress output", action='store_true')
     args = parser.parse_args()
+    
+    # TODO custom clang-format executable
     
     # We have to do it like this because the append action doesn't clear the default list
     if not args.directory:
@@ -135,6 +178,6 @@ if __name__ == "__main__":
         if args.command == "check":
             status |= test.check(set(args.directory), args.quiet)
         elif args.command == "fix":
-            test.fix(set(args.directory), args.quiet)
+            status |= test.fix(set(args.directory), args.quiet)
                 
     sys.exit(status)
