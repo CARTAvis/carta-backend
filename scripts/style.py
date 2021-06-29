a0bf8627ae0370c7f4961fd1a074ebc72448e344
Adrianna Pińska <adrianna.pinska@gmail.com>
2021-06-30 01:17:41 +0200

    Don't define the default executable in two places

--- scripts/style.py
+++ scripts/style.py
@@ -201,7 +201,7 @@ if __name__ == "__main__":
     parser.add_argument('test', help=f"Test to perform (or all tests; style is currently excluded from all).", choices=(*Test.TESTS.keys(), "all"))
     parser.add_argument('command', help="Command to perform.", choices=("check", "fix"))
     parser.add_argument('-q', '--quiet', help="Suppress output", action='store_true')
-    parser.add_argument('--clang-format', help="Path to custom clang-format executable.", default="clang-format")
+    parser.add_argument('--clang-format', help="Path to custom clang-format executable.", default=Format.executable)
     args = parser.parse_args()
         
     if args.quiet:
