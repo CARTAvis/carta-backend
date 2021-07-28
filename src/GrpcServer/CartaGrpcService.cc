241bd65d4153ea8c37760e5c1f07af322efc28bf
Adrianna Pińska <adrianna.pinska@gmail.com>
2021-07-28 19:45:05 +0200

    basic implementation of an unencrypted security token sent in the message metadata

--- src/GrpcServer/CartaGrpcService.cc
+++ src/GrpcServer/CartaGrpcService.cc
@@ -21,7 +21,7 @@
 
 uint32_t CartaGrpcService::_scripting_request_id = 0;
 
-CartaGrpcService::CartaGrpcService() {}
+CartaGrpcService::CartaGrpcService(std::string auth_token) : _auth_token(auth_token) {}
 
 void CartaGrpcService::AddSession(Session* session) {
     // Map session to its ID, set connected to false
@@ -47,9 +47,22 @@ grpc::Status CartaGrpcService::CallAction(
     auto parameters = request->parameters();
     auto async = request->async();
 
+    auto metadata = context->client_metadata();
+
+    std::string token;
+
+    for (auto& m : metadata) {
+        if (m.first == "token") {
+            token = std::string(m.second.begin(), m.second.size());
+            break;
+        }
+    }
+
     grpc::Status status(grpc::Status::OK);
 
-    if (_sessions.find(session_id) == _sessions.end()) {
+    if (token != _auth_token) {
+        status = grpc::Status(grpc::StatusCode::PERMISSION_DENIED, "Invalid token.");
+    } else if (_sessions.find(session_id) == _sessions.end()) {
         status = grpc::Status(grpc::StatusCode::OUT_OF_RANGE, fmt::format("Invalid session ID {}.", session_id));
     } else {
         _scripting_request_id++;
