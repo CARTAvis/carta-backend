c54b2cb7ec71820e2bd399f42f5f9ea47879ddf9
Cheng-Chin Chiang <chcchiang@asiaa.sinica.edu.tw>
2021-09-22 15:03:50 +0800

    Refactor messages handlers on the Main and OnMessageTask (#920)
    * refactor messages handlers on the Main and OnMessageTask
    
    * simplify the code for the warning log when fails to parse a message
    
    * move a template class GeneralMessageTask to a new file OnMessageTask.tcc

--- src/OnMessageTask.cc
+++ src/OnMessageTask.cc
@@ -7,85 +7,15 @@
 #include "OnMessageTask.h"
 
 #include <algorithm>
-#include <cstring>
-
-#include "EventHeader.h"
-#include "Logger/Logger.h"
-#include "Util.h"
-
-tbb::task* MultiMessageTask::execute() {
-    switch (_header.type) {
-        case CARTA::EventType::SET_SPATIAL_REQUIREMENTS: {
-            CARTA::SetSpatialRequirements message;
-            if (message.ParseFromArray(_event_buffer, _event_length)) {
-                _session->OnSetSpatialRequirements(message);
-            } else {
-                spdlog::warn("Bad SET_SPATIAL_REQUIREMENTS message!");
-            }
-            break;
-        }
-        case CARTA::EventType::SET_STATS_REQUIREMENTS: {
-            CARTA::SetStatsRequirements message;
-            if (message.ParseFromArray(_event_buffer, _event_length)) {
-                _session->OnSetStatsRequirements(message);
-            } else {
-                spdlog::warn("Bad SET_STATS_REQUIREMENTS message!");
-            }
-            break;
-        }
-        case CARTA::EventType::MOMENT_REQUEST: {
-            CARTA::MomentRequest message;
-            if (message.ParseFromArray(_event_buffer, _event_length)) {
-                _session->OnMomentRequest(message, _header.request_id);
-            } else {
-                spdlog::warn("Bad MOMENT_REQUEST message!");
-            }
-            break;
-        }
-        case CARTA::EventType::FILE_LIST_REQUEST: {
-            CARTA::FileListRequest message;
-            if (message.ParseFromArray(_event_buffer, _event_length)) {
-                _session->OnFileListRequest(message, _header.request_id);
-            } else {
-                spdlog::warn("Bad FILE_LIST_REQUEST message!");
-            }
-            break;
-        }
-        case CARTA::EventType::REGION_LIST_REQUEST: {
-            CARTA::RegionListRequest message;
-            if (message.ParseFromArray(_event_buffer, _event_length)) {
-                _session->OnRegionListRequest(message, _header.request_id);
-            } else {
-                spdlog::warn("Bad REGION_LIST_REQUEST message!");
-            }
-            break;
-        }
-        case CARTA::EventType::CATALOG_LIST_REQUEST: {
-            CARTA::CatalogListRequest message;
-            if (message.ParseFromArray(_event_buffer, _event_length)) {
-                _session->OnCatalogFileList(message, _header.request_id);
-            } else {
-                spdlog::warn("Bad CATALOG_LIST_REQUEST message!");
-            }
-            break;
-        }
-        default: {
-            spdlog::warn("Bad event type in MultiMessageType:execute : ({})", _header.type);
-            break;
-        }
-    }
-
-    return nullptr;
-}
 
 tbb::task* SetImageChannelsTask::execute() {
     std::pair<CARTA::SetImageChannels, uint32_t> request_pair;
     bool tester;
 
-    _session->ImageChannelLock(fileId);
-    tester = _session->_set_channel_queues[fileId].try_pop(request_pair);
-    _session->ImageChannelTaskSetIdle(fileId);
-    _session->ImageChannelUnlock(fileId);
+    _session->ImageChannelLock(_file_id);
+    tester = _session->_set_channel_queues[_file_id].try_pop(request_pair);
+    _session->ImageChannelTaskSetIdle(_file_id);
+    _session->ImageChannelUnlock(_file_id);
 
     if (tester) {
         _session->ExecuteSetChannelEvt(request_pair);
@@ -96,15 +26,6 @@ tbb::task* SetImageChannelsTask::execute() {
 
 tbb::task* SetCursorTask::execute() {
     _session->_file_settings.ExecuteOne("SET_CURSOR", _file_id);
-    return nullptr;
-}
-
-tbb::task* SetHistogramRequirementsTask::execute() {
-    CARTA::SetHistogramRequirements message;
-    if (message.ParseFromArray(_event_buffer, _event_length)) {
-        _session->OnSetHistogramRequirements(message, _header.request_id);
-    }
-
     return nullptr;
 }
 
@@ -121,17 +42,6 @@ tbb::task* AnimationTask::execute() {
             _session->CancelAnimation();
         }
     }
-
-    return nullptr;
-}
-
-tbb::task* OnAddRequiredTilesTask::execute() {
-    _session->OnAddRequiredTiles(_message, _session->AnimationRunning());
-    return nullptr;
-}
-
-tbb::task* OnSetContourParametersTask::execute() {
-    _session->OnSetContourParameters(_message);
     return nullptr;
 }
 
@@ -147,10 +57,5 @@ tbb::task* SpectralProfileTask::execute() {
 
 tbb::task* OnSplataloguePingTask::execute() {
     _session->OnSplataloguePing(_request_id);
-    return nullptr;
-}
-
-tbb::task* OnSpectralLineRequestTask::execute() {
-    _session->OnSpectralLineRequest(_message, _request_id);
     return nullptr;
 }
---
+++ src/OnMessageTask.tcc
@@ -0,0 +1,48 @@
+/* This file is part of the CARTA Image Viewer: https://github.com/CARTAvis/carta-backend
+   Copyright 2018, 2019, 2020, 2021 Academia Sinica Institute of Astronomy and Astrophysics (ASIAA),
+   Associated Universities, Inc. (AUI) and the Inter-University Institute for Data Intensive Astronomy (IDIA)
+   SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+#ifndef CARTA_BACKEND__ONMESSAGETASK_TCC_
+#define CARTA_BACKEND__ONMESSAGETASK_TCC_
+
+template <typename T>
+class GeneralMessageTask : public OnMessageTask {
+    tbb::task* execute() {
+        if constexpr (std::is_same_v<T, CARTA::SetHistogramRequirements>) {
+            _session->OnSetHistogramRequirements(_message, _request_id);
+        } else if constexpr (std::is_same_v<T, CARTA::AddRequiredTiles>) {
+            _session->OnAddRequiredTiles(_message, _session->AnimationRunning());
+        } else if constexpr (std::is_same_v<T, CARTA::SetContourParameters>) {
+            _session->OnSetContourParameters(_message);
+        } else if constexpr (std::is_same_v<T, CARTA::SpectralLineRequest>) {
+            _session->OnSpectralLineRequest(_message, _request_id);
+        } else if constexpr (std::is_same_v<T, CARTA::SetSpatialRequirements>) {
+            _session->OnSetSpatialRequirements(_message);
+        } else if constexpr (std::is_same_v<T, CARTA::SetStatsRequirements>) {
+            _session->OnSetStatsRequirements(_message);
+        } else if constexpr (std::is_same_v<T, CARTA::MomentRequest>) {
+            _session->OnMomentRequest(_message, _request_id);
+        } else if constexpr (std::is_same_v<T, CARTA::FileListRequest>) {
+            _session->OnFileListRequest(_message, _request_id);
+        } else if constexpr (std::is_same_v<T, CARTA::RegionListRequest>) {
+            _session->OnRegionListRequest(_message, _request_id);
+        } else if constexpr (std::is_same_v<T, CARTA::CatalogListRequest>) {
+            _session->OnCatalogFileList(_message, _request_id);
+        } else {
+            spdlog::warn("Bad event type for GeneralMessageTask!");
+        }
+        return nullptr;
+    };
+
+    T _message;
+    uint32_t _request_id;
+
+public:
+    GeneralMessageTask(Session* session, T message, uint32_t request_id)
+        : OnMessageTask(session), _message(message), _request_id(request_id) {}
+    ~GeneralMessageTask() = default;
+};
+
+#endif // CARTA_BACKEND__ONMESSAGETASK_TCC_
