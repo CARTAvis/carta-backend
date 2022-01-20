3ec0a450e550779b442cb3f5239d15962d4fce9a
Jorge Lopez <jorgext@gmail.com>
2022-01-20 15:03:12 +0100

    fix formatting

--- src/Main/Main.cc
+++ src/Main/Main.cc
@@ -51,7 +51,8 @@ int main(int argc, char* argv[]) {
             exit(0);
         }
 
-        carta::logger::InitLogger(settings.no_log, settings.verbosity, settings.log_performance, settings.log_protocol_messages, settings.user_directory);
+        carta::logger::InitLogger(
+            settings.no_log, settings.verbosity, settings.log_performance, settings.log_protocol_messages, settings.user_directory);
         settings.FlushMessages(); // flush log messages produced during Program Settings setup
 
         if (settings.wait_time >= 0) {
