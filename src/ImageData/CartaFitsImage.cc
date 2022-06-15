21519b8c1f32081d792b5c7b109f2fad44f4cfd6
Cheng-Chin Chiang <chengchin.chiang@gmail.com>
2022-06-15 23:35:59 +0800

    Revert "set _fptr as a nullptr in the CartaFitsImage copy constructor"
    This reverts commit 6f92c80ad517b7a74039b41792ef832237416cf2.

--- src/ImageData/CartaFitsImage.cc
+++ src/ImageData/CartaFitsImage.cc
@@ -35,7 +35,8 @@ CartaFitsImage::CartaFitsImage(const std::string& filename, unsigned int hdu)
       _is_compressed(false),
       _datatype(casacore::TpOther),
       _has_blanks(false),
-      _pixel_mask(nullptr) {
+      _pixel_mask(nullptr),
+      _is_copy(false) {
     casacore::File ccfile(filename);
     if (!ccfile.exists() || !ccfile.isReadable()) {
         throw(casacore::AipsError("FITS file is not readable or does not exist."));
@@ -48,21 +49,24 @@ CartaFitsImage::CartaFitsImage(const CartaFitsImage& other)
     : ImageInterface<float>(other),
       _filename(other._filename),
       _hdu(other._hdu),
-      _fptr(nullptr),
+      _fptr(other._fptr),
       _shape(other._shape),
       _is_compressed(other._is_compressed),
       _datatype(other._datatype),
       _has_blanks(other._has_blanks),
       _pixel_mask(nullptr),
-      _tiled_shape(other._tiled_shape) {
+      _tiled_shape(other._tiled_shape),
+      _is_copy(true) {
     if (other._pixel_mask != nullptr) {
         _pixel_mask = other._pixel_mask->clone();
     }
 }
 
 CartaFitsImage::~CartaFitsImage() {
-    CloseFile();
-    delete _pixel_mask;
+    if (!_is_copy) {
+        CloseFile();
+        delete _pixel_mask;
+    }
 }
 
 // Image interface
