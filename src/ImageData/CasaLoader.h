c7f6e054d4d74c5e07283e14d1bfb7ee8affb2f7
Pam Harris <pharris@nrao.edu>
2022-07-14 18:16:19 -0600

    Check CASA image lock file can be read-locked to prevent hang

--- src/ImageData/CasaLoader.h
+++ src/ImageData/CasaLoader.h
@@ -7,11 +7,13 @@
 #ifndef CARTA_BACKEND_IMAGEDATA_CASALOADER_H_
 #define CARTA_BACKEND_IMAGEDATA_CASALOADER_H_
 
+#include <casacore/casa/IO/LockFile.h>
 #include <casacore/images/Images/ImageOpener.h>
 #include <casacore/images/Images/PagedImage.h>
 #include <casacore/images/Images/TempImage.h>
 
 #include "FileLoader.h"
+#include "Logger/Logger.h"
 
 namespace carta {
 
@@ -29,6 +31,17 @@ CasaLoader::CasaLoader(const std::string& filename) : FileLoader(filename) {}
 
 void CasaLoader::OpenFile(const std::string& /*hdu*/) {
     if (!_image) {
+        // Check if image is locked; PagedImage AutoNoReadLocking option tries to acquire a lock and blocks until it is free
+        bool create(false), add_request(false), must_exist(false);
+        auto lock_file =
+            std::unique_ptr<casacore::LockFile>(new casacore::LockFile(_filename + "/table.lock", 5, create, add_request, must_exist));
+
+        if (!lock_file->canLock(casacore::FileLocker::Read)) {
+            throw(casacore::AipsError("Cannot open image, locked by another process."));
+        }
+
+        lock_file.reset(nullptr);
+
         bool converted(false);
 
         try {
