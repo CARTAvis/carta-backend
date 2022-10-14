0117880480a956bfa974d63f582dcd89f1051f4a
Cheng-Chin Chiang <chengchin.chiang@gmail.com>
2022-10-15 00:30:39 +0800

    Modify the TestLoadingFile and add a timer for raster tile data generation

--- test-perf/TestLoadingFile.cc
+++ test-perf/TestLoadingFile.cc
@@ -7,23 +7,31 @@
 #include <omp.h>
 
 #include "CommonTestUtilities.h"
+#include "Frame/VectorFieldCalculator.h"
 #include "ImageData/FileLoader.h"
 #include "TestFrame.h"
+#include "ThreadingManager/ThreadingManager.h"
 #include "Timer/Timer.h"
 
 using namespace carta;
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    // Set logger
-    fs::path user_directory("");
-    int verbosity(5);
-    logger::InitLogger(true, verbosity, true, false, user_directory);
-
-    if (argc != 3) {
-        spdlog::error("Usage: ./TestLoadingFile <full path name of the image file> <omp thread count, -1 means auto selected>");
+    if (argc != 4) {
+        spdlog::error(
+            "Usage: "
+            "./TestLoadingFile "
+            "<full path name of the image file> "
+            "<omp thread count, -1 means auto selected> "
+            "<verbosity, 4: info, 5:debug> ");
         return 1;
     }
+
+    // Set logger
+    fs::path user_directory("");
+    string verbosity_string(argv[3]);
+    int verbosity = stoi(verbosity_string);
+    logger::InitLogger(true, verbosity, true, false, user_directory);
 
     // Set image file name path
     string path_string(argv[1]);
@@ -33,21 +41,21 @@ int main(int argc, char* argv[]) {
     int omp_thread_count = stoi(omp_thread_count_str);
     if (omp_thread_count > 0) {
         omp_set_num_threads(omp_thread_count);
-        spdlog::info("OMP thread numbers {}", omp_thread_count);
+        spdlog::debug("OMP thread numbers {}", omp_thread_count);
     } else {
         omp_set_num_threads(omp_get_num_procs());
-        spdlog::info("OMP thread numbers {}", omp_get_num_procs());
+        spdlog::debug("OMP thread numbers {}", omp_get_num_procs());
     }
 
-    // Set file loader
-    Timer t;
+    // Set FileLoader
+    carta::Timer t;
     shared_ptr<FileLoader> loader(FileLoader::GetLoader(path_string));
-    spdlog::info("Elapsed time to load an image file for a loader: {} (us)", t.Elapsed().us());
+    spdlog::info("Elapsed time to load an image file for FileLoader: {} (us)", t.Elapsed().us());
 
     // Set Frame
     t.Reset();
     unique_ptr<Frame> frame(new Frame(0, loader, "0"));
-    spdlog::info("Elapsed time to load an image file for a frame: {} (us)", t.Elapsed().us());
+    spdlog::info("Elapsed time to load an image file for Frame: {} (us)", t.Elapsed().us());
 
     if (!frame->IsValid()) {
         spdlog::error("Failed to open the image file!");
@@ -62,6 +70,39 @@ int main(int argc, char* argv[]) {
     spdlog::info("Elapsed time to calculate the image histogram: {} (us)", t.Elapsed().us());
 
     EXPECT_TRUE(image_histogram);
+
+    // Set required tiles
+    int mip(2);
+    std::vector<Tile> tiles;
+    GetTiles(frame->Width(), frame->Height(), mip, tiles);
+    int num_tiles = tiles.size();
+    spdlog::debug("Number of tiles: {}", num_tiles);
+    EXPECT_GT(num_tiles, 0);
+
+    // Set required channel, stokes, compression type, and compression quality
+    int z(0);
+    int stokes(0);
+    CARTA::CompressionType compression_type = CARTA::CompressionType::ZFP;
+    float compression_quality(14.0);
+
+    // Do raster tile data generation and compression
+    t.Reset();
+#pragma omp parallel
+    {
+        int num_threads = omp_get_num_threads();
+        int stride = std::min(num_tiles, std::min(num_threads, MAX_TILING_TASKS));
+#pragma omp for
+        for (int j = 0; j < stride; j++) {
+            for (int i = j; i < num_tiles; i += stride) {
+                auto raster_tile_data = Message::RasterTileData(0, 0);
+                auto tile = tiles[i];
+                if (!frame->FillRasterTileData(raster_tile_data, tile, z, stokes, compression_type, compression_quality)) {
+                    spdlog::warn("Discarding stale tile request for channel={}, layer={}, x={}, y={}", z, tile.layer, tile.x, tile.y);
+                }
+            }
+        }
+    }
+    spdlog::info("Elapsed time to generate raster tile data: {} (us)", t.Elapsed().us());
 
     return 0;
 }
