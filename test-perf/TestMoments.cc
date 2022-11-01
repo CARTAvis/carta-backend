e711198e8a8f6f32e27a66a2894ace4fefe23fd0
Cheng-Chin Chiang <chengchin.chiang@gmail.com>
2022-11-02 00:59:47 +0800

    Add a moments performance tester

---
+++ test-perf/TestMoments.cc
@@ -0,0 +1,107 @@
+/* This file is part of the CARTA Image Viewer: https://github.com/CARTAvis/carta-backend
+   Copyright 2018-2022 Academia Sinica Institute of Astronomy and Astrophysics (ASIAA),
+   Associated Universities, Inc. (AUI) and the Inter-University Institute for Data Intensive Astronomy (IDIA)
+   SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+#include <omp.h>
+
+#include "CommonTestUtilities.h"
+#include "Frame/VectorFieldCalculator.h"
+#include "ImageData/FileLoader.h"
+#include "InitTester.h"
+#include "TestFrame.h"
+#include "ThreadingManager/ThreadingManager.h"
+#include "Timer/Timer.h"
+
+using namespace carta;
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc != 5) {
+        spdlog::error(
+            "Usage: "
+            "./TestRegion "
+            "<full path name of the image file> "
+            "<omp thread count, -1 means auto selected> "
+            "<verbosity, 4: info, 5:debug> "
+            "<region width/height, -1 means equal to the image width and height>");
+        return 1;
+    }
+
+    // Set image file name path
+    string path_string(argv[1]);
+
+    // Set logger
+    InitSpdlog(argv[3]);
+
+    // Set OMP thread numbers
+    InitOmpThreads(argv[2]);
+
+    // Set region width/height
+    string region_width_height(argv[4]);
+    int region_size = stoi(region_width_height);
+
+    // Set FileLoader
+    shared_ptr<FileLoader> loader(FileLoader::GetLoader(path_string));
+
+    // Set Frame
+    uint32_t session_id(0);
+    shared_ptr<Frame> frame = make_shared<Frame>(session_id, loader, "0");
+
+    if (!frame->IsValid()) {
+        spdlog::error("Failed to open the image file!");
+        return 1;
+    }
+
+    // Set image channels through the Frame
+    std::string message;
+    int channel(0), stokes(0);
+    frame->SetImageChannels(channel, stokes, message);
+
+    // Create a region handler
+    auto region_handler = std::make_unique<carta::RegionHandler>();
+
+    // Set a rectangle region state: // [(cx,cy), (width,height)], width/height > 0
+    int region_id(1);
+    int image_width = frame->Width();
+    int image_height = frame->Height();
+    int center_x(image_width / 2);
+    int center_y(image_height / 2);
+    int region_width = region_size > 0 ? region_size : image_width;
+    int region_height = region_size > 0 ? region_size : image_height;
+    spdlog::debug("Set region width/height: {}/{}", region_width, region_height);
+    std::vector<CARTA::Point> points = {
+        Message::Point(center_x, center_y), Message::Point(region_width, region_height)}; // {center, width/height}
+
+    int ref_file_id(0);
+    RegionState region_state(ref_file_id, CARTA::RegionType::RECTANGLE, points, 0);
+    EXPECT_TRUE(region_handler->SetRegion(region_id, region_state, frame->CoordinateSystem()));
+
+    // Set spectral configs for a rectangle region
+    std::string stokes_config_z("z");
+    int file_id(0);
+    std::vector<CARTA::SetSpectralRequirements_SpectralConfig> spectral_configs{Message::SpectralConfig(stokes_config_z)};
+    region_handler->SetSpectralRequirements(region_id, file_id, frame, spectral_configs);
+
+    // Set region moment request
+    auto spectral_range = Message::IntBounds(0, frame->Depth() - 1);
+    auto pixel_range = Message::FloatBounds(0, 1);
+    auto moment_request =
+        Message::MomentsRequest(file_id, region_id, CARTA::MomentAxis::SPECTRAL, CARTA::MomentMask::None, spectral_range, pixel_range);
+
+    // Set region moment response
+    auto progress_callback = [&](float progress) {};
+    CARTA::MomentResponse moment_response;
+    std::vector<GeneratedImage> collapse_results;
+
+    // Do moments calculation
+    carta::Timer t;
+    EXPECT_TRUE(
+        region_handler->CalculateMoments(file_id, region_id, frame, progress_callback, moment_request, moment_response, collapse_results));
+    spdlog::info("Elapsed time to calculate region moments: {} (ms)", t.Elapsed().ms());
+
+    EXPECT_EQ(collapse_results.size(), moment_request.moments().size() - 1);
+
+    return 0;
+}
