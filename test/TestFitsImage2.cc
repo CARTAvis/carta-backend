4d7fc02c7fb16614099076e0bd3262c6532fb369
Cheng-Chin Chiang <chengchin.chiang@gmail.com>
2023-01-16 14:35:18 +0800

    Change testing codes and add more test cases

--- test/TestFitsImage2.cc
+++ test/TestFitsImage2.cc
@@ -11,22 +11,16 @@
 #include "ImageData/FileLoader.h"
 #include "Timer/Timer.h"
 
-#define TEXT_COLOR "\033[1m\033[44m"
-#define RESET "\033[0m\n"
-
 using namespace carta;
 
-class FitsImageTest2 : public ::testing::Test, public ImageGenerator {};
-
 // Load a FITS image and get 2D slice data via the "fits_read_pix" function
-int UsePureCfitsio(string file_path, vector<float>& image_data) {
+int UsePureCfitsio(string file_path, int dims, vector<float>& image_data) {
     int status = 0;
     fitsfile* fptr;
 
     // Open image and get data size. File is assumed to be 2D
     fits_open_file(&fptr, file_path.c_str(), READONLY, &status);
 
-    int dims = 4;
     std::vector<long> image_size(dims);
     std::vector<long> start_pix(dims);
 
@@ -46,9 +40,9 @@ int UsePureCfitsio(string file_path, vector<float>& image_data) {
     double dt = t.Elapsed().us();
 
     double pixel_per_t = (double)image_size[0] * (double)image_size[1] / dt;
-    auto message =
-        fmt::format("[CFITSIO] For {}x{} plane, number of pixels per unit time: {:.3f} MPix/s", image_size[0], image_size[1], pixel_per_t);
-    std::cout << TEXT_COLOR << message << RESET;
+    auto message = fmt::format(
+        "[CFITSIO] For {}x{} image data, number of pixels per unit time: {:.3f} MPix/s", image_size[0], image_size[1], pixel_per_t);
+    std::cout << message << endl;
 
     fits_close_file(fptr, &status);
 
@@ -58,46 +52,72 @@ int UsePureCfitsio(string file_path, vector<float>& image_data) {
     return status;
 }
 
-TEST_F(FitsImageTest2, BasicLoadingTest) {
-    // Generate a FITS image
-    auto file_path = GeneratedFitsImagePath("1000 1000 1 1");
+class FitsImageTest2 : public ::testing::Test, public ImageGenerator {
+public:
+    void Load2DSliceData(const vector<int>& shape, bool compressed) {
+        // Generate a FITS image
+        string image_shape;
+        for (int i = 0; i < shape.size(); ++i) {
+            image_shape += to_string(shape[i]);
+            if (i != shape.size() - 1) {
+                image_shape += ' ';
+            }
+        }
+        auto file_path = GeneratedFitsImagePath(image_shape);
 
-    // Compress the FITS image
-    string cmd = "gzip " + file_path;
-    system(cmd.c_str());
-    file_path += ".gz";
+        if (compressed) {
+            // Compress the FITS image
+            string cmd = "gzip " + file_path;
+            system(cmd.c_str());
+            file_path += ".gz";
+        }
 
-    // Load a compressed FITS image
-    std::shared_ptr<carta::FileLoader> loader(carta::FileLoader::GetLoader(file_path));
-    auto frame = std::make_unique<Frame>(0, loader, "0");
+        // Load a compressed FITS image
+        std::shared_ptr<carta::FileLoader> loader(carta::FileLoader::GetLoader(file_path));
+        auto frame = std::make_unique<Frame>(0, loader, "0");
 
-    int width = frame->Width();
-    int height = frame->Height();
-    int channel = 0;
-    int stokes = frame->CurrentStokes();
+        int width = frame->Width();
+        int height = frame->Height();
+        int channel = 0;
+        int stokes = frame->CurrentStokes();
 
-    StokesSlicer stokes_slicer = frame->GetImageSlicer(AxisRange(0, width - 1), AxisRange(0, height - 1), AxisRange(channel), stokes);
-    auto image_data_size = stokes_slicer.slicer.length().product();
-    auto image_data = std::make_unique<float[]>(image_data_size);
+        StokesSlicer stokes_slicer = frame->GetImageSlicer(AxisRange(0, width - 1), AxisRange(0, height - 1), AxisRange(channel), stokes);
+        auto image_data_size = stokes_slicer.slicer.length().product();
+        auto image_data = std::make_unique<float[]>(image_data_size);
 
-    // Check the elapsed time to get a 2D slice data
-    Timer t;
-    EXPECT_TRUE(frame->GetSlicerData(stokes_slicer, image_data.get()));
-    double dt = t.Elapsed().us();
+        // Check the elapsed time to get a 2D slice data
+        Timer t;
+        EXPECT_TRUE(frame->GetSlicerData(stokes_slicer, image_data.get()));
+        double dt = t.Elapsed().us();
 
-    EXPECT_TRUE((image_data_size == width * height));
+        EXPECT_TRUE((image_data_size == width * height));
 
-    double pixel_per_t = width * height / dt;
-    auto message = fmt::format("[CARTA] For {}x{} plane, number of pixels per unit time: {:.3f} MPix/s", width, height, pixel_per_t);
+        double pixel_per_t = width * height / dt;
+        auto message =
+            fmt::format("[CARTA] For {}x{} image data, number of pixels per unit time: {:.3f} MPix/s", width, height, pixel_per_t);
 
-    std::cout << TEXT_COLOR << message << RESET;
+        std::cout << message << endl;
 
-    // Check the consistency of image data getting by two ways
-    vector<float> image_data1;
-    GetVectorData(image_data1, image_data.get(), image_data_size);
+        // Check the consistency of image data getting by two ways
+        vector<float> image_data1;
+        GetVectorData(image_data1, image_data.get(), image_data_size);
 
-    vector<float> image_data2;
-    UsePureCfitsio(file_path, image_data2);
+        // Get 2D slice data with pure cfitsio
+        vector<float> image_data2;
+        UsePureCfitsio(file_path, shape.size(), image_data2);
 
-    CmpVectors(image_data1, image_data2);
+        CmpVectors(image_data1, image_data2);
+    }
+};
+
+TEST_F(FitsImageTest2, LoadCompressed2DSliceData) {
+    Load2DSliceData({500, 500, 1, 1}, true);
+    Load2DSliceData({1000, 1000, 1, 1}, true);
+    Load2DSliceData({2000, 2000, 1, 1}, true);
+}
+
+TEST_F(FitsImageTest2, Load2DSliceData) {
+    Load2DSliceData({500, 500, 1, 1}, false);
+    Load2DSliceData({1000, 1000, 1, 1}, false);
+    Load2DSliceData({2000, 2000, 1, 1}, false);
 }
