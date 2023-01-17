47eca281e966a2e640aa3de739bb351cec339b88
Adrianna Pińska <adrianna.pinska@gmail.com>
2023-01-17 10:42:59 +0200

    Add Doxygen documentation (#1225)
    * Initial setup of Doxygen with custom stylesheet
    
    * strip full path from files
    
    * Tweaked settings to expose more of the code in the docs (e.g. private members)
    
    * Put back implementation files, which were removed to try to make Breathe happy
    
    * Shortened project description; tried unsuccessfully to fix dark mode
    
    * Made diagrams transparent for better dark mode support (needs colour tweaks?)
    
    * Updated changelog
    
    * Extract all entities, and generate call and caller graphs
    
    * Added a main page
    
    * Added a static markdown page
    
    * fixed typo
    
    * Added example documentation for TileCache, TileCacheKey and TilePool.
    
    * The action we're going to use really wants to run doxygen in the repo root, so that's where the Doxyfile needs to be.
    
    * Added a section to the FAQ about building with ASAN flags.
    
    * Added doxygen workflow tested in fits2idia

--- src/Cache/TileCacheKey.h
+++ src/Cache/TileCacheKey.h
@@ -8,21 +8,46 @@
 #define CARTA_BACKEND_TILECACHEKEY_H
 
 namespace carta {
+/** @brief Key for tiles used in TileCache
+ *  @details This is used in TileCache to identify tiles.
+ *  @see std::hash<carta::TileCacheKey>
+ */
 struct TileCacheKey {
+    /** @brief Default constructor */
     TileCacheKey() {}
+    /** @brief Constructor from tile coordinates
+     *  @param x The X coordinate of the tile
+     *  @param y The Y coordinate of the tile
+     */
     TileCacheKey(int32_t x, int32_t y) : x(x), y(y) {}
+    /** @brief Equality operator
+     *  @param other Another key
+     *  @return Whether the keys are equal
+     *  @details Keys are equal if their coordinates are equal.
+     */
     bool operator==(const TileCacheKey& other) const {
         return (x == other.x && y == other.y);
     }
+    /** @brief The X coordinate */
     int32_t x;
+    /** @brief The Y coordinate */
     int32_t y;
 };
 
 } // namespace carta
 
 namespace std {
+/** @brief Hash for carta::TileCacheKey
+ *  @details This allows carta::TileCacheKey to be used as a key in the unordered map in carta::TileCache.
+ *  @see carta::TileCacheKey
+ */
 template <>
 struct hash<carta::TileCacheKey> {
+    /** @brief The operator which calculates the hash
+     *  @param k The key
+     *  @return The key hash
+     *  @details The hash is calculated from the hashes of the coordinates of the key.
+     */
     std::size_t operator()(const carta::TileCacheKey& k) const {
         return std::hash<int32_t>()(k.x) ^ (std::hash<int32_t>()(k.y) << 1);
     }
