0fd6777fa328221c0172a6eb3ddf96acaf862ff7
Cheng-Chin Chiang <chengchin.chiang@gmail.com>
2023-06-05 11:45:06 +0800

    Fix the problem of memory leakage

--- src/ImageData/FitsLoader.h
+++ src/ImageData/FitsLoader.h
@@ -32,7 +32,7 @@ private:
 
     int GetNumHeaders(const std::string& filename, int hdu);
     void RemoveHistoryBeam(unsigned int hdu_num);
-    bool Is64BitFitsBeamTable(const std::string& filename);
+    bool Is64BitBeamTable(const std::string& filename);
 };
 
 FitsLoader::FitsLoader(const std::string& filename, bool is_gz) : FileLoader(filename, "", is_gz) {}
@@ -101,7 +101,7 @@ void FitsLoader::OpenFile(const std::string& hdu) {
                     _image.reset(new casacore::FITSImage(_unzip_file, 0, hdu_num));
                 }
             } else if (use_casacore_fits) {
-                if (Is64BitFitsBeamTable(_filename)) {
+                if (Is64BitBeamTable(_filename)) {
                     use_casacore_fits = false;
                     _image.reset(new CartaFitsImage(_filename, hdu_num));
                 } else {
@@ -213,7 +213,7 @@ void FitsLoader::RemoveHistoryBeam(unsigned int hdu_num) {
     }
 }
 
-bool FitsLoader::Is64BitFitsBeamTable(const std::string& filename) {
+bool FitsLoader::Is64BitBeamTable(const std::string& filename) {
     fitsfile* fptr;
     int status(0);
     fits_open_file(&fptr, filename.c_str(), 0, &status);
@@ -236,24 +236,33 @@ bool FitsLoader::Is64BitFitsBeamTable(const std::string& filename) {
         return false;
     }
 
-    // Read columns into vectors
+    // Get BEAMS columns data type
     int casesen(CASEINSEN), colnum(0);
-    char ttype, tunit, tform, nulstr, tdisp;
+    char* ttype = (char*)malloc(sizeof(char*));
+    char* tform = (char*)malloc(sizeof(char*));
+    char tunit, nulstr, tdisp;
     long tbcol;
     double scale, zero;
+
+    auto close_file = [&]() {
+        fits_close_file(fptr, &status);
+        free(ttype);
+        free(tform);
+    };
 
     std::vector<std::string> keys = {"BMAJ", "BMIN", "BPA", "CHAN", "POL"};
     for (auto& key : keys) {
         status = 0;
         fits_get_colnum(fptr, casesen, key.data(), &colnum, &status);
-        fits_get_acolparms(fptr, colnum, &ttype, &tbcol, &tunit, &tform, &scale, &zero, &nulstr, &tdisp, &status);
-        if (tform == 'K' || tform == 'D') {
+        fits_get_acolparms(fptr, colnum, ttype, &tbcol, &tunit, tform, &scale, &zero, &nulstr, &tdisp, &status);
+        if (*tform == 'K' || *tform == 'D') { // 'K' is 64-bit long signed integer, and 'D' is double precision float
+            close_file();
             spdlog::warn("BEAMS table consists of 64-bit parameters. Convert these values to 32-bit.");
             return true;
         }
     }
 
-    fits_close_file(fptr, &status);
+    close_file();
     return false;
 }
 
