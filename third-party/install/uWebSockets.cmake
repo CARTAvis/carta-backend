227d35213ec1ea6ecd3636a19eaa90acc1e079a8
Cheng-Chin Chiang <chengchin.chiang@gmail.com>
2023-09-08 09:58:11 +0800

    Use FetchContent instead of ExternalProject to download and build uSockets

--- third-party/install/uWebSockets.cmake
+++ third-party/install/uWebSockets.cmake
@@ -1,32 +1,51 @@
 macro(install_uWebSockets)
-    INCLUDE(ExternalProject)
+    INCLUDE(FetchContent)
 
-    # Build uSocket project
+    # Build uSocket
     SET(USOCKETS_SOURCE_DIR ${CMAKE_SOURCE_DIR}/third-party/uSockets)
 
-    ExternalProject_Add(uSockets-build
-            URL               https://github.com/uNetworking/uSockets/archive/refs/tags/v0.8.6.zip
-            SOURCE_DIR        ${USOCKETS_SOURCE_DIR}
-            CONFIGURE_COMMAND ""
-            BUILD_COMMAND     cd ${USOCKETS_SOURCE_DIR} && make
-            INSTALL_COMMAND   "")
+    FetchContent_Declare(
+            uSockets-build
+            URL        https://github.com/uNetworking/uSockets/archive/refs/tags/v0.8.6.zip
+            SOURCE_DIR ${USOCKETS_SOURCE_DIR}
+    )
+
+    FetchContent_GetProperties(uSockets-build)
+    if (NOT uSockets-build_POPULATED)
+        FetchContent_Populate(uSockets-build)
+    endif()
 
     INCLUDE_DIRECTORIES(${USOCKETS_SOURCE_DIR}/src)
 
-    ADD_LIBRARY(uSockets STATIC IMPORTED)
-    SET_TARGET_PROPERTIES(uSockets PROPERTIES IMPORTED_LOCATION ${USOCKETS_SOURCE_DIR}/uSockets.a)
+    AUX_SOURCE_DIRECTORY(${USOCKETS_SOURCE_DIR}/src USOCKETS_FILES)
+    AUX_SOURCE_DIRECTORY(${USOCKETS_SOURCE_DIR}/src/crypto USOCKETS_CRYPTO_FILES)
+    AUX_SOURCE_DIRECTORY(${USOCKETS_SOURCE_DIR}/src/eventing USOCKETS_EVENTING_FILES)
+    AUX_SOURCE_DIRECTORY(${USOCKETS_SOURCE_DIR}/src/internal USOCKETS_INTERNAL_FILES)
+    AUX_SOURCE_DIRECTORY(${USOCKETS_SOURCE_DIR}/src/io_uring USOCKETS_IO_URING_FILES)
 
-    # Build uWebSockets project
+    SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DLIBUS_NO_SSL")
+
+    ADD_LIBRARY(uSockets
+            ${USOCKETS_FILES}
+            ${USOCKETS_CRYPTO_FILES}
+            ${USOCKETS_EVENTING_FILES}
+            ${USOCKETS_INTERNAL_FILES}
+            ${USOCKETS_IO_URING_FILES})
+
+    # Build uWebSockets
     SET(UWEBSOCKETS_SOURCE_DIR ${CMAKE_SOURCE_DIR}/third-party/uWebSockets)
 
-    ExternalProject_Add(uWebSockets-build
-            URL               https://github.com/uNetworking/uWebSockets/archive/refs/tags/v20.46.0.zip
-            SOURCE_DIR        ${UWEBSOCKETS_SOURCE_DIR}
-            CONFIGURE_COMMAND ""
-            BUILD_COMMAND     ""
-            INSTALL_COMMAND   "")
+    FetchContent_Declare(
+            uWebSockets-build
+            URL        https://github.com/uNetworking/uWebSockets/archive/refs/tags/v20.46.0.zip
+            SOURCE_DIR ${UWEBSOCKETS_SOURCE_DIR}
+    )
+
+    FetchContent_GetProperties(uWebSockets-build)
+    if (NOT uWebSockets-build_POPULATED)
+        FetchContent_Populate(uWebSockets-build)
+    endif()
 
     INCLUDE_DIRECTORIES(${UWEBSOCKETS_SOURCE_DIR}/src)
 
 endmacro()
-
