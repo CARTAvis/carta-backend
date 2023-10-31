450c4bf54346397003e7fe6fd584c50158d14153
Cheng-Chin Chiang <chengchin.chiang@gmail.com>
2023-10-31 16:02:10 +0800

    Modify the bash script check_guard_name_style.sh

--- scripts/check_guard_name_style.sh
+++ scripts/check_guard_name_style.sh
@@ -2,7 +2,7 @@
 
 # Execute this bash script in the current folder, and it will normalize the guard names from all header files
 
-function CheckGuardNameStyle () {
+function CheckGuardNameStyle() {
   filename=$1
 
   # Get the guard name
@@ -12,8 +12,7 @@ function CheckGuardNameStyle () {
   sub_names=(${filename//\// })
 
   # Concatenate the sub-names of the file path name
-  for i in "${sub_names[@]}"
-  do
+  for i in "${sub_names[@]}"; do
     guard_name+=$i
     guard_name+="_"
   done
@@ -28,9 +27,12 @@ function CheckGuardNameStyle () {
   line_num=$(grep -m1 -n -- '^#ifndef' "${filename}")
   line_num="${line_num%%:*}"
 
+  # Get bash environment name
+  uname_out="$(uname -s)"
+
   if [[ $line_num -gt 0 ]]; then
     # Replace with the guard name in the line number for the first occurrence of '#ifndef ...'
-    if [[ "${OSTYPE}" == "darwin"* ]]; then
+    if [[ "${uname_out}" == "Darwin" ]]; then
       sed -i '' "${line_num}s/.*/#ifndef ${guard_name}/" "${filename}"
     else
       sed -i "${line_num}s/.*/#ifndef ${guard_name}/" "${filename}"
@@ -40,7 +42,7 @@ function CheckGuardNameStyle () {
     line_num=$((line_num + 1))
 
     # Replace with the guard name in the line number after the first occurrence of '#ifndef ...'
-    if [[ "${OSTYPE}" == "darwin"* ]]; then
+    if [[ "${uname_out}" == "Darwin" ]]; then
       sed -i '' "${line_num}s/.*/#define ${guard_name}/" "${filename}"
     else
       sed -i "${line_num}s/.*/#define ${guard_name}/" "${filename}"
@@ -48,7 +50,7 @@ function CheckGuardNameStyle () {
   fi
 
   # Replace the last occurrence of matched '#endif ...'
-  if [[ "${OSTYPE}" == "darwin"* ]]; then
+  if [[ "${uname_out}" == "Darwin" ]]; then
     sed -i '' "$ s/^#endif .*$/#endif \/\/ ${guard_name}/" "${filename}"
   else
     sed -i "$ s/^#endif .*$/#endif \/\/ ${guard_name}/" "${filename}"
@@ -62,19 +64,47 @@ function CheckFolder() {
   filenames=($(find "${folder}" -name "${suffix}"))
 
   # Replace the guard names for each of the files
-  for j in "${filenames[@]}"
-  do
+  for j in "${filenames[@]}"; do
     filename=$j
     CheckGuardNameStyle "${filename}"
   done
 }
 
-# Go to the base folder
-cd ..
+function GetBaseFolder() {
+  local str=$(pwd)
+  local substr="carta-backend"
 
-# Do replacements
-CheckFolder "src" "*.h"
-CheckFolder "src" "*.tcc"
+  # reverse strings
+  local reverse_str=$(echo $str | rev)
+  local reverse_substr=$(echo $substr | rev)
 
-CheckFolder "test" "*.h"
-CheckFolder "test" "*.tcc"
+  # find index of reversed substring in reversed string
+  local prefix=${reverse_str%%$reverse_substr*}
+  local reverse_index=${#prefix}
+
+  # calculate last index
+  local index=$((${#str} - $reverse_index))
+
+  if [[ index -lt 0 ]]; then
+    echo ""
+  else
+    echo "${str:0:index}"
+  fi
+}
+
+# Get the carta-backend base folder
+base_folder=$(GetBaseFolder)
+
+if [ -z "${base_folder}" ]; then
+  echo "Can not find the carta backend base folder!"
+else
+  # Go to the base folder
+  cd ${base_folder}
+
+  # Do replacements
+  CheckFolder "src" "*.h"
+  CheckFolder "src" "*.tcc"
+
+  CheckFolder "test" "*.h"
+  CheckFolder "test" "*.tcc"
+fi
