78bd8cc68810e9db647b64ce980a0a202e68c16a
Cheng-Chin Chiang <chengchin.chiang@gmail.com>
2023-11-29 15:48:01 +0800

    Fix a bug

--- src/Main/Main.cc
+++ src/Main/Main.cc
@@ -52,7 +52,7 @@ int main(int argc, char* argv[]) {
             exit(0);
         }
 
-        carta::logger::InitLogger();
+        carta::logger::InitLogger(settings);
         settings.FlushMessages(); // flush log messages produced during Program Settings setup
 
         // Send casacore log messages (global and local) to sink.
