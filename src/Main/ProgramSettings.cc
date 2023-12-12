205e856c9d6c77d28eb3a2bc2f74805b48108643
Cheng-Chin Chiang <chengchin.chiang@gmail.com>
2023-12-12 22:52:17 +0800

    Modify the description of the command line parameter for full image caching

--- src/Main/ProgramSettings.cc
+++ src/Main/ProgramSettings.cc
@@ -193,7 +193,7 @@ void ProgramSettings::ApplyCommandLineSettings(int argc, char** argv) {
         ("files", "files to load", cxxopts::value<std::vector<string>>(positional_arguments))
         ("no_user_config", "ignore user configuration file", cxxopts::value<bool>())
         ("no_system_config", "ignore system configuration file", cxxopts::value<bool>())
-        ("full_image_cache_size", "maximum amount of memory (in MB)", cxxopts::value<int>(), "<MB>");
+        ("full_image_cache_size", "maximum amount of memory for full image caching in MB", cxxopts::value<int>());
 
     options.add_options("Deprecated and debug")
         ("debug_no_auth", "accept all incoming WebSocket connections on the specified port(s) (not secure; use with caution!)", cxxopts::value<bool>())
