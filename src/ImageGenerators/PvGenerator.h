392d41b6217df48d991695d400abf4093ad387cf
Pam Harris <pharris@nrao.edu>
2024-06-07 09:48:12 -0600

    Use distance axis for PV image from polyline

--- src/ImageGenerators/PvGenerator.h
+++ src/ImageGenerators/PvGenerator.h
@@ -20,27 +20,32 @@ namespace carta {
 
 class PvGenerator {
 public:
+    enum PositionAxisType {OFFSET, DISTANCE};
+
     PvGenerator();
     ~PvGenerator() = default;
 
     // For PV generator (not preview)
     void SetFileName(int index, const std::string& filename, bool is_preview = false);
 
-    // Create generated PV image from input data. If reverse, [spectral, offset] instead of normal [offset, spectral].
+    // Create generated PV image from input data.
+    // cut_region_type determines position axis as offset from center (line) or distance from beginning (polyline).
+    // reverse determines order of position and spectral axes.
     // Returns generated image and success, with message if failure.
     bool GetPvImage(const std::shared_ptr<Frame>& frame, const casacore::Matrix<float>& pv_data, casacore::IPosition& pv_shape,
-        const casacore::Quantity& offset_increment, int start_chan, int stokes, bool reverse, GeneratedImage& pv_image,
-        std::string& message);
+        PositionAxisType axis_type, const casacore::Quantity& position_increment, int start_chan, int stokes, bool reverse,
+        GeneratedImage& pv_image, std::string& message);
 
 private:
     void SetPvImageName(const std::string& filename, int index, bool is_preview);
 
     std::shared_ptr<casacore::ImageInterface<casacore::Float>> SetupPvImage(
         const std::shared_ptr<casacore::ImageInterface<float>>& input_image, const std::shared_ptr<casacore::CoordinateSystem>& input_csys,
-        casacore::IPosition& pv_shape, int stokes, const casacore::Quantity& offset_increment, double spectral_refval, bool reverse,
-        std::string& message);
+        casacore::IPosition& pv_shape, const std::string& position_name, const casacore::Quantity& position_increment,
+        double position_refval, double position_refpix, double spectral_refval, int stokes, bool reverse, std::string& message);
     casacore::CoordinateSystem GetPvCoordinateSystem(const std::shared_ptr<casacore::CoordinateSystem>& input_csys,
-        casacore::IPosition& pv_shape, int stokes, const casacore::Quantity& offset_increment, double spectral_refval, bool reverse);
+        casacore::IPosition& pv_shape, const std::string& position_name, const casacore::Quantity& position_increment,
+        double position_refval, double position_refpix, double spectral_refval, int stokes, bool reverse);
 
     // GeneratedImage parameters
     int _file_id;
