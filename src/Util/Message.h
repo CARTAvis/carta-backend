22efce80f716f14a97c4a704c52f4da334677633
Pam Harris <pharris@nrao.edu>
2024-11-07 15:38:31 -0700

    Update ICD version to match protobuf

--- src/Util/Message.h
+++ src/Util/Message.h
@@ -48,7 +48,7 @@
 #include "ImageStats/Histogram.h"
 
 namespace carta {
-const uint16_t ICD_VERSION = 28;
+const uint16_t ICD_VERSION = 30;
 struct EventHeader {
     uint16_t type;
     uint16_t icd_version;
