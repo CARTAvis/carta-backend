471a527a4f8cac7f0b8692038925789c7bf2550e
Adrianna Pińska <adrianna.pinska@gmail.com>
2025-02-10 12:26:12 +0200

    Aggregate information about image axes and dimensions into structs (#1392)
    * Use Z axis rather than spectral axis to offset slicer before accessing channel cache
    
    * Added fix to changelog
    
    * get correct XY axes from file when creating Z/Stokes slicer for region
    
    * Use render axes and depth axis in polarization calculator instead of spatial axes and spectral axis. Pass values already calculated in loader instead of repeating calculations.
    
    * Aggregate dimensions and axes into structs in Frame and FileLoader
    
    * fixed merge error
    
    * Use existing dims in pol calc instead of recalculating them
    
    * Formatting pass
    
    * removed seldom-used render axes property, to avoid having to sync it with x and y
    
    * refactor axes and dims structs for a more consistent and OO interface; refactor use of axes and dims in ext info loader
    
    * Remove unused spectral and stokes axis properties from compressed fits
    
    * removed stray cout
    
    * Set in/out parameter correctly

--- src/ImageData/PolarizationCalculator.h
+++ src/ImageData/PolarizationCalculator.h
@@ -23,7 +23,7 @@ class PolarizationCalculator {
     enum StokesTypes { I, Q, U, V };
 
 public:
-    PolarizationCalculator(std::shared_ptr<casacore::ImageInterface<float>> image, std::vector<int> axes, AxisRange z_range,
+    PolarizationCalculator(std::shared_ptr<casacore::ImageInterface<float>> image, AxesInfo axes, DimsInfo dims, AxisRange z_range,
         AxisRange x_range, AxisRange y_range);
     ~PolarizationCalculator() = default;
 
