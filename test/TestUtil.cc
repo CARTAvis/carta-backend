17839a867b1c875343efe8b56b85953c75356982
Pam Harris <pharris@nrao.edu>
2025-04-04 09:20:31 -0600

    Add casacore-modified GILDAS units to test

--- test/TestUtil.cc
+++ test/TestUtil.cc
@@ -115,6 +115,8 @@ TEST(UtilTest, CheckGildasUnit) {
     EXPECT_TRUE(IsGildasUnit("K (Ta*)"));
     EXPECT_TRUE(IsGildasUnit("K (Tmb)"));
     EXPECT_TRUE(IsGildasUnit("Jy (Tb)"));
+    EXPECT_TRUE(IsGildasUnit("K.(Ta.)"));
+    EXPECT_TRUE(IsGildasUnit("K.(Tmb)"));
     EXPECT_FALSE(IsGildasUnit("K"));
     EXPECT_FALSE(IsGildasUnit("Jy"));
 }
