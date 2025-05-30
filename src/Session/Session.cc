eb12f904928c856a5ac7296bbcfb0f76f45d32b5
Adrianna Pińska <adrianna.pinska@gmail.com>
2025-05-30 12:45:48 +0200

    fix loading region files from top-level directory by removing unnecessary directory check

--- src/Session/Session.cc
+++ src/Session/Session.cc
@@ -944,7 +944,7 @@ void Session::OnImportRegion(const CARTA::ImportRegion& message, uint32_t reques
         std::vector<std::string> contents = {message.contents().begin(), message.contents().end()};
 
         // check for file or contents set
-        bool import_file(!directory.empty() && !filename.empty()), import_contents(!contents.empty());
+        bool import_file(!filename.empty()), import_contents(!contents.empty());
         if (!import_file && !import_contents) {
             auto import_ack = Message::ImportRegionAck(false, "Import region failed: cannot import by filename or contents.");
             SendFileEvent(file_id, CARTA::EventType::IMPORT_REGION_ACK, request_id, import_ack);
