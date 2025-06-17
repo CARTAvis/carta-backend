7736244be7ddeb63df2e054585901faebd811a49
confluence <adrianna.pinska@gmail.com>
2025-06-17 07:52:41 +0000

    Deploying to gh-pages from @ CARTAvis/carta-backend@f2af32c8256fd48ec91f1cd68e415cf3c554e75d 🚀

--- StatsCalculator_8h.js
+++ StatsCalculator_8h.js
@@ -2,5 +2,9 @@ var StatsCalculator_8h =
 [
     [ "CalcBasicStats", "StatsCalculator_8h.html#a96f7cab56516141317895bada342c0b1", null ],
     [ "CalcHistogram", "StatsCalculator_8h.html#a3d967873206a1f0ff9062a2ecf29ae6b", null ],
-    [ "CalcStatsValues", "StatsCalculator_8h.html#a385968df288a71e88ec0fecc88dd07bf", null ]
+    [ "CalcStatsValues", "StatsCalculator_8h.html#ad170096e224b353fb6640af99aa07cd2", null ],
+    [ "ComputeFluxDensity", "StatsCalculator_8h.html#abb062a6fb38ecb29210058e4e4417b7b", null ],
+    [ "GetBeamArea", "StatsCalculator_8h.html#a68704e5ff32840645f473a1f5fb801a8", null ],
+    [ "GetPositionStats", "StatsCalculator_8h.html#ad8b192f665b49ae5cafe68ce415e9b30", null ],
+    [ "carta_stats_to_casacore", "StatsCalculator_8h.html#a558a4b2833be3a1ee6039f6dcce54971", null ]
 ];
