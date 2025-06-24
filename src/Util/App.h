2198e8d878b15bbcc7bab95f215842aef6964886
Adrianna Pińska <adrianna.pinska@gmail.com>
2025-06-24 15:25:09 +0200

    Update version to rc in preparation for 5.0 release

--- src/Util/App.h
+++ src/Util/App.h
@@ -10,7 +10,7 @@
 #include <string>
 
 // version
-#define VERSION_ID "5.0.0-beta.1"
+#define VERSION_ID "5.0.0-rc.1"
 
 /**
  * @brief Retrieves the absolute path of the currently running executable.
