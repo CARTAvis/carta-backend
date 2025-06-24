f940e5b240a7639e6fc6d329ee44cb32c5337e8c
Adrianna Pińska <adrianna.pinska@gmail.com>
2025-06-24 15:39:12 +0200

    corrected rc version -- from now on we always start at .0

--- src/Util/App.h
+++ src/Util/App.h
@@ -10,7 +10,7 @@
 #include <string>
 
 // version
-#define VERSION_ID "5.0.0-rc.1"
+#define VERSION_ID "5.0.0-rc.0"
 
 /**
  * @brief Retrieves the absolute path of the currently running executable.
