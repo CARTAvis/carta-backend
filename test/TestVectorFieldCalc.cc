6eb737eb2f2e7081c899440ebc7bee1e832b966d
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2025-11-12 14:51:27 +0800

    updated test to some initial version which works with Stokes I image (all ones)

--- test/TestVectorFieldCalc.cc
+++ test/TestVectorFieldCalc.cc
@@ -37,7 +37,7 @@ class VectorFieldCalcTest : public ::testing::Test, public ImageGenerator {};
 TEST_F(VectorFieldCalcTest, BasicStartTest) {
 // TODO : how to generate images with 4 different values for IQUV - add option to make_image.py or create image myself ?
 //    auto path_string = GeneratedFitsImagePath("2048 2048 1 4"); // 4 Stokes 
-    std::string path_string = "~/github/CARTA/code/branches/issue1526/carta-backend/test/four_stokes.fits";
+    std::string path_string = "/home/msok/github/CARTA/code/branches/issue1526/carta-backend/test/four_stokes.fits";
     std::shared_ptr<carta::FileLoader> loader(carta::FileLoader::GetLoader(path_string));
     EXPECT_NE(loader.get(), nullptr);    
     std::unique_ptr<Frame> frame(new Frame(0, loader, "0"));
@@ -68,32 +68,21 @@ TEST_F(VectorFieldCalcTest, BasicStartTest) {
           // TileData : image_data()
           EXPECT_EQ( message.intensity_tiles_size(), 1 );
           EXPECT_EQ( message.angle_tiles_size(), 1 );
+
+          // check angle tiles :
           const CARTA::TileData& tile_data = message.angle_tiles(0); // message.intensity_tiles(0);
-          std::string image_data = tile_data.image_data();
+          const std::string& image_data = tile_data.image_data();
           const float* float_data = (const float*)image_data.c_str(); // static_cast<const float*>(image_data.c_str());
           std::cout << "DEBUG : image_data len = " << image_data.length() << " test value = " << image_data[0] << " float values = " << float_data[0] << " , " << float_data[1] << std::endl; // not const value ??? even compressed in some way should be constant, right ?
           
-          // vs: Compression.cc 
-          // int Decompress(std::vector<float>& array, std::vector<char>& compression_buffer, int nx, int ny, int precision);
-          std::vector<float> tile_data_float;
-          std::vector<char>  tile_data_char( image_data.length() );
-          memcpy(tile_data_char.data(),image_data.c_str(),image_data.length() );
-          if(  Decompress( tile_data_float, tile_data_char, tile_data.width(), tile_data.height(), 100 ) ){
-             std::cout << "WARNING : decompression failed !" << std::endl;
+          int float_size = image_data.size()/4;
+          for(int i=0;i<float_size;i++){
+              EXPECT_NEAR( float_data[i] , 1 , 1e-8f);
           }
-          std::cout << "DEBUG2 : " << " char values = "  << tile_data_char[0] << " , " << tile_data_char[1] << std::endl;    // these have some values 
-          std::cout << "DEBUG2 : " << " float values = " << tile_data_float[0] << " , " << tile_data_float[1] << std::endl;  // these seem to be all zeros ??? hmm...
           
-// ???          
-// QUESTION : how to decode these message to check if the values in IQUV images match 1,2,3,4 as in the generated image ???
-// ???
- 
-// Both these crash with similar message :
-//    unknown file: Failure
-//     C++ exception with description "Failed to parse message" thrown in the test body.         
-//
-//          CARTA::TileData decoded_tile_data = Message::DecodeMessage<CARTA::TileData>( image_data );
-//          CARTA::VectorOverlayTileData decoded_tile_data = Message::DecodeMessage<CARTA::VectorOverlayTileData>( image_data );
+          // how to check pa, pi etc :
+          
+          
        };
        
     frame->CalculateVectorField( callback );   
