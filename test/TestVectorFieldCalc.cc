119f6f09c7a60cbff24f2a52b2b34c15254ad986
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2025-11-12 11:30:34 +0800

    added draft of the test case - still awaiting decoding of messages and checking correctness of the final values. Plus the test FITS file has not been added anywhere and only exists on my laptop.

---
+++ test/TestVectorFieldCalc.cc
@@ -0,0 +1,89 @@
+/* This file is part of the CARTA Image Viewer: https://github.com/CARTAvis/carta-backend
+   Copyright 2018- Academia Sinica Institute of Astronomy and Astrophysics (ASIAA),
+   Associated Universities, Inc. (AUI) and the Inter-University Institute for Data Intensive Astronomy (IDIA)
+   SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+#include <gtest/gtest.h>
+
+#include "ImageData/FileLoader.h"
+#include "src/Frame/Frame.h"
+
+#include "CommonTestUtilities.h"
+
+using namespace carta;
+
+// Allows testing of protected methods in Frame without polluting the original class
+// was public ::testing::Test
+class TestFrame : public Frame {
+public:
+    TestFrame(uint32_t session_id, std::shared_ptr<carta::FileLoader> loader, const std::string& hdu, int default_z = DEFAULT_Z)
+        : Frame(session_id, loader, hdu, default_z) {}
+        
+//    void SetUp() override {
+//       std::cout << "DEBUG : TestFrame::SetUp" << std::endl;
+//    }
+
+    void callback(CARTA::VectorOverlayTileData& message)
+    {
+       std::cout << "DEBUG : received a message" << std::endl;
+    }
+        
+    FRIEND_TEST(FitsImageTest, ExampleFriendTest);
+};
+
+class VectorFieldCalcTest : public ::testing::Test, public ImageGenerator {};
+
+TEST_F(VectorFieldCalcTest, BasicStartTest) {
+// TODO : how to generate images with 4 different values for IQUV - add option to make_image.py or create image myself ?
+//    auto path_string = GeneratedFitsImagePath("2048 2048 1 4"); // 4 Stokes 
+    std::string path_string = "~/github/CARTA/code/branches/issue1526/carta-backend/test/four_stokes.fits";
+    std::shared_ptr<carta::FileLoader> loader(carta::FileLoader::GetLoader(path_string));
+    EXPECT_NE(loader.get(), nullptr);    
+    std::unique_ptr<Frame> frame(new Frame(0, loader, "0"));
+    EXPECT_NE(frame.get(), nullptr);
+    EXPECT_TRUE(frame->IsValid());
+    
+//    char szCmd[1024];
+//    sprintf(szCmd,"cp %s /tmp/fits/",path_string.c_str());
+//    system(szCmd);
+
+//    std::string hdu{"/home/msok/github/CARTA/out.fits"};
+//    std::string hdu{ path_string };
+//    cout << "DEBUG : reading image " << hdu << std::endl;
+//    TestFrame test_frame(1, loader, hdu );
+
+    CARTA::SetVectorOverlayParameters param_message;
+    param_message.set_smoothing_factor(2); // this was required as otherwise it returns on ClearParameters
+    // TODO : add more set_ calls ???
+    
+    frame->SetVectorOverlayParameters( param_message );
+    
+    auto callback = [](CARTA::VectorOverlayTileData& message){
+          std::cout << "DEBUG : received a message intensity tile size = " << message.intensity_tiles_size() << " , angle tile size = " << message.angle_tiles_size() << std::endl;
+          
+          // TODO : decode message and add to vector to be checked later :
+          // Decode in Util/Message.h ??? but it takes string ...
+          // How to get data values out of message ???
+          // TileData : image_data()
+          EXPECT_EQ( message.intensity_tiles_size(), 1 );
+          EXPECT_EQ( message.angle_tiles_size(), 1 );
+          const CARTA::TileData& tile_data = message.intensity_tiles(0);
+          std::string image_data = tile_data.image_data();
+          std::cout << "DEBUG : test value = " << image_data[0] << std::endl; // not const value ??? even compressed in some way should be constant, right ?
+
+// ???          
+// QUESTION : how to decode these message to check if the values in IQUV images match 1,2,3,4 as in the generated image ???
+// ???
+ 
+// Both these crash with similar message :
+//    unknown file: Failure
+//     C++ exception with description "Failed to parse message" thrown in the test body.         
+//
+//          CARTA::TileData decoded_tile_data = Message::DecodeMessage<CARTA::TileData>( image_data );
+//          CARTA::VectorOverlayTileData decoded_tile_data = Message::DecodeMessage<CARTA::VectorOverlayTileData>( image_data );
+       };
+       
+    frame->CalculateVectorField( callback );   
+}
+
