02bab3cab0b4e2493f6ada06cdf68b2fe398a110
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2025-11-26 19:50:20 +0800

    using message as test parameter - but still need to work out the test logic itself

--- test/TestVectorFieldCalc.cc
+++ test/TestVectorFieldCalc.cc
@@ -11,14 +11,24 @@
 
 using namespace carta;
 
-enum class TestVectorFieldType {
+/*enum class TestVectorFieldType {
     TestStokesI,
     TestStokesPi,
     TestStokesPa
+};*/
+
+class SetVectorOverlayParametersTest : public CARTA::SetVectorOverlayParameters {
+public :
+    SetVectorOverlayParametersTest(int intensity=1, int angle=0) : CARTA::SetVectorOverlayParameters() {
+       set_smoothing_factor(2);
+       
+       set_stokes_intensity(intensity);
+       set_stokes_angle(angle);
+    }
 };
 
 // Define the parameterized test fixture
-class VectorFieldCalcParamTest : public ::testing::TestWithParam<TestVectorFieldType> {
+class VectorFieldCalcParamTest : public ::testing::TestWithParam<SetVectorOverlayParametersTest> {
 public:
     VectorFieldCalcParamTest() {}
      
@@ -30,76 +40,57 @@ protected:
 
 class TestVectorField : public VectorFieldCalculator {
 public:
-   TestVectorField(const CARTA::SetVectorOverlayParameters& message, int stokes_axis) : VectorFieldCalculator(message, stokes_axis) {}
+   TestVectorField(const SetVectorOverlayParametersTest& message, bool has_stokes_axis) : VectorFieldCalculator(message, has_stokes_axis) {}
 };
 
 
 TEST_P(VectorFieldCalcParamTest, TestStokes) {
-    CARTA::SetVectorOverlayParameters param_message;
-    param_message.set_smoothing_factor(2); // this was required as otherwise it returns on ClearParameters
-
     double expected_value = 1.00;
-    TestVectorFieldType test_type = GetParam();
-    switch( test_type ){
-       case TestVectorFieldType::TestStokesI :
-          expected_value = 1.00;
-          break;
-
-       case TestVectorFieldType::TestStokesPi :
-          {
-             VectorFieldCalculator::CalcPi calcpi(0.0f,0.0f);
-             expected_value = calcpi(2,3);
-             param_message.set_stokes_intensity(1);
-          }
-          break;
-          
-       case TestVectorFieldType::TestStokesPa :
-          {
-             VectorFieldCalculator::CalcPa calcpa;             
-             expected_value = calcpa(2,3);
-             param_message.set_stokes_angle(1);
-          }
-          break;
-          
-       default : 
-          expected_value = -1;
-    }
-    std::cout << "DEBUG : expected value = " << expected_value << std::endl;
-
-    TestVectorField vectorfield( param_message, 4 );
+    SetVectorOverlayParametersTest test_parameters = GetParam();
     
-    // lambda expression receiving tile data (messege as sent to front-end) and checking if all values = 1 (as expected for Stokes I)
-    auto callback = [&expected_value,&test_type](CARTA::VectorOverlayTileData& message){
-          // std::cout << "DEBUG : received a message intensity tile size = " << message.intensity_tiles_size() << " , angle tile size = " << message.angle_tiles_size() << std::endl;          
-          EXPECT_EQ( message.intensity_tiles_size(), 1 );
-          EXPECT_EQ( message.angle_tiles_size(), 1 );
-
-          // check angle tiles :
-          const CARTA::TileData* tile_data = &(message.intensity_tiles(0)); // message.angle_tiles(0);
-          if( test_type == TestVectorFieldType::TestStokesPa ){
-             tile_data = &(message.angle_tiles(0));
-          }
-          const std::string& image_data = tile_data->image_data();
-          const float* float_data = reinterpret_cast<const float*>(image_data.c_str()); // static_cast<const float*>(image_data.c_str());
-          // std::cout << "DEBUG : image_data len = " << image_data.length() << " test value = " << image_data[0] << " float values = " << float_data[0] << " , " << float_data[1] << std::endl; // not const value ??? even compressed in some way should be constant, right ?
-          
-          int float_size = image_data.size()/4;
-          for(int i=0;i<float_size;i++){
-              EXPECT_NEAR( float_data[i] , expected_value , 1e-8f);
-          }
-       };
-
     std::unordered_map<CARTA::PolarizationType, int> stokes_test_values_map{ {CARTA::PolarizationType::POLARIZATION_TYPE_NONE, 1}, 
                                                                              {CARTA::PolarizationType::I, 1}, 
                                                                              {CARTA::PolarizationType::Q, 2}, 
                                                                              {CARTA::PolarizationType::U, 3}, 
                                                                              {CARTA::PolarizationType::V, 4}
                                                                            }; 
+          
+    TestVectorField vectorfield( test_parameters, true );
+    
+    // lambda expression receiving tile data (messege as sent to front-end) and checking if all values = 1 (as expected for Stokes I)
+    auto callback = [&test_parameters,&stokes_test_values_map](CARTA::VectorOverlayTileData& message){
+          // std::cout << "DEBUG : received a message intensity tile size = " << message.intensity_tiles_size() << " , angle tile size = " << message.angle_tiles_size() << std::endl;          
+          EXPECT_EQ( message.intensity_tiles_size(), 1 );
+          EXPECT_EQ( message.angle_tiles_size(), 1 );
+
+          // check intensity tiles :
+          float expected_intensity = stokes_test_values_map[CARTA::PolarizationType::I];
+          const float* float_data = reinterpret_cast<const float*>(message.intensity_tiles(0).image_data().c_str()); // static_cast<const float*>(image_data.c_str());
+          int float_size = message.angle_tiles(0).image_data().size()/4;
+          
+          // TODO - this part still fails for some reason - I do not understand what's going on in this logic 
+          for(int i=0;i<float_size;i++){
+              EXPECT_NEAR( float_data[i] , expected_intensity, 1e-8f);
+          }
+          
+          // check angle tiles :
+          if( message.angle_tiles_size() > 0 ) {
+              VectorFieldCalculator::CalcPa calcpa;
+              float expected_angle = calcpa( stokes_test_values_map[CARTA::PolarizationType::Q] , stokes_test_values_map[CARTA::PolarizationType::U] );
+              float_data = reinterpret_cast<const float*>(message.angle_tiles(0).image_data().c_str());
+              float_size = message.angle_tiles(0).image_data().size()/4;
+              for(int i=0;i<float_size;i++){
+                  EXPECT_NEAR( float_data[i] , expected_angle, 1e-8f);
+              }
+          }
+       };
+
        
     // lambda expression producing tile data (256x256) all set to 1 for Stokes I        
-    auto getdata_callback = [&stokes_test_values_map,&test_type](std::vector<float>& data, CARTA::ImageBounds& bounds, int smoothing_factor, CARTA::PolarizationType stokes_type, int& width, int& height)
+    auto getdata_callback = [&stokes_test_values_map](std::vector<float>& data, CARTA::ImageBounds& bounds, int smoothing_factor, CARTA::PolarizationType stokes_type, int& width, int& height)
     {             
        int value = stokes_test_values_map[stokes_type]; // seems that Stokes I is passed as Current 
+       std::cout << "DEBUG : getdata_callback stokes = " << stokes_type << " value = " << value << std::endl;
        
        data.assign(256*256,value); // generating Stokes I tile 256x256 all values = 1 
        width = 256;
@@ -122,7 +113,7 @@ TEST_P(VectorFieldCalcParamTest, TestStokes) {
 INSTANTIATE_TEST_SUITE_P(
     StokesTests,
     VectorFieldCalcParamTest,
-    ::testing::Values(TestVectorFieldType::TestStokesI, TestVectorFieldType::TestStokesPi, TestVectorFieldType::TestStokesPa)
+    ::testing::Values(SetVectorOverlayParametersTest(0,0)) // TODO : add also parameters to check Pi and Stokes I 
 );
 
 
