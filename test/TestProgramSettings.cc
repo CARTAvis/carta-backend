64e3f8d6564b760182ab44ac2455a3a0952b6aa7
Michaela van Zyl <michaela@maholla.com>
2025-12-10 12:21:47 +0200

    missing /

--- test/TestProgramSettings.cc
+++ test/TestProgramSettings.cc
@@ -420,5 +420,5 @@ TEST_F(ProgramSettingsTest, TestFileQueryStringTwoFilesDifferentFolder) {
 
     auto url_string = carta::HttpServer::GetFileUrlString(files);
     EXPECT_EQ(url_string,
-        "files=" + SafeStringEscape(fmt::format("{}/{},{}/{}", folder_fits, "noise_3d.fits", folder_hdf5, "noise_10px_10px.hdf5")));
+        "files=" + SafeStringEscape(fmt::format("/{}/{},{}/{}", folder_fits, "noise_3d.fits", folder_hdf5, "noise_10px_10px.hdf5")));
 }
