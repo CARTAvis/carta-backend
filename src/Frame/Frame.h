f5ca99f68e8477a8da100759b4e3d7c5699adfa6
Adrianna Pińska <adrianna.pinska@gmail.com>
2026-01-15 13:48:42 +0200

    Backport cache vector allocation and core dump fix (#1549)
    * Allocate memory for image cache once. (#1508)
    
    * Allocate memory for image cache once. Image size should be size_t. Check for nullptr to see if cache filled.
    
    * added to changelog
    
    * Added channel and polarization indices to performance log for image cache fill
    
    * Moved cache allocation back to fill function, but made it conditional. Fixed bad logic in cache validity checks.
    
    * restored original constructor code
    
    * attempt to remove image cache from core dump (#1506)
    
    * attempt to remove image cache from core dump
    
    * Check for platform compatibility before using madvise
    
    * fix included headers; formatting pass
    
    * Added error handling
    
    * working solution with aligned memory; needs tidying up
    
    * Encapsulated page alignment and madvise functionality in custom alias and function. Added changelog and docstrings.
    
    * Fixed docstring issues
    
    * fixed typo in docstring
    
    * Added a basic unit test.
    
    ---------
    
    Co-authored-by: Adrianna Pińska <adrianna.pinska@gmail.com>
    
    ---------
    
    Co-authored-by: Angus Comrie <accomrie@gmail.com>

--- src/Frame/Frame.h
+++ src/Frame/Frame.h
@@ -32,6 +32,7 @@
 #include "ThreadingManager/Concurrency.h"
 #include "Util/FileSystem.h"
 #include "Util/Image.h"
+#include "Util/Memory.h"
 #include "Util/Message.h"
 
 namespace carta {
@@ -296,8 +297,8 @@ protected:
     ContourSettings _contour_settings;
 
     // Image data cache and mutex
-    long long int _image_cache_size;
-    std::unique_ptr<float[]> _image_cache;
+    size_t _image_cache_size;
+    UniqueAlignedDataPtr<float> _image_cache;
     bool _image_cache_valid;       // cached image data is valid for current z and stokes
     queuing_rw_mutex _cache_mutex; // allow concurrent reads but lock for write
     std::mutex _image_mutex;       // only one disk access at a time
