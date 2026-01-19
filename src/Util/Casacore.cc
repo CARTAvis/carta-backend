b062b8d62f7c3153c20407fd95c5f87486a1acee
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2026-01-19 16:09:48 +0800

    message style and formatting made consistent with other places. Path added in message generated in the exception catch code.

--- src/Util/Casacore.cc
+++ src/Util/Casacore.cc
@@ -32,17 +32,17 @@ casacore::String GetResolvedFilename(
 
     // Check directory
     if (!cc_file.exists()) {
-        message = "Directory " + path.expandedName() + " does not exist.";
+        message = fmt::format("Directory {} does not exist.", path.expandedName());
     } else if (!cc_file.isReadable()) {
-        message = "Directory " + path.expandedName() + " is not readable.";
+        message = fmt::format("Directory {} is not readable.", path.expandedName());
     } else {
         // Check file
         path.append(file);
         cc_file = casacore::File(path);
         if (!cc_file.exists()) {
-            message = "File " + path.expandedName() + " does not exist.";
+            message = fmt::format("File {} does not exist.", path.expandedName());
         } else if (!cc_file.isReadable()) {
-            message = "File " + path.expandedName() + " is not readable.";
+            message = fmt::format("File {} is not readable.", path.expandedName());
         } else {
             try {
                 resolved_filename = path.resolvedName();
@@ -52,7 +52,7 @@ casacore::String GetResolvedFilename(
                 if (path.absoluteName().empty()) {
                     resolved_filename = path.expandedName();
                 } else {
-                    message = err.getMesg();
+                    fmt::format("Error resolving file {} : {}", path.expandedName(), err.getMesg());
                 }
             }
         }
