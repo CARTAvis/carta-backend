087d87c2c4431d1219d37f221cc72b913b829093
Michaela van Zyl <michaela@maholla.com>
2026-02-13 16:27:25 +0200

    format fix

--- src/Util/Message.cc
+++ src/Util/Message.cc
@@ -667,7 +667,7 @@ CARTA::ImportRegionAck Message::ImportRegionAck(bool success, const std::string&
     return import_region_ack;
 }
 
-CARTA::RegionInfo Message::SetRegionInfo(CARTA::RegionType& region_type,  float rotation) {
+CARTA::RegionInfo Message::SetRegionInfo(CARTA::RegionType& region_type, float rotation) {
     CARTA::RegionInfo regionInfo;
     regionInfo.set_region_type(region_type);
     regionInfo.set_rotation(rotation);
