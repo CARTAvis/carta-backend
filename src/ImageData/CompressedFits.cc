303fb02e3c56e676d4791327c2ada28281a9f839
Michaela van Zyl <36291167+Michaelazzz@users.noreply.github.com>
2026-03-03 13:47:37 +0200

    Protobuf header message creation cleanup and helper functions (#1458)
    * SpatialProfileData function cleanup
    
    * removed unused SetRegion function
    
    * remove unused SetStatsRequirements function
    
    * SpectralProfileData function cleanup
    
    * removing comments
    
    * format fix
    
    * FileListResponse AddDirectory function
    
    * AddComutedEntry helper function
    
    * AddImportedRegion helper function
    
    * Fixed AddImportedRegion
    
    * AddProfile helper and comments
    
    * format fix
    
    * small fixes
    
    * format fix
    
    * return statement
    
    * SpatialProfileData change
    
    * format fix
    
    * minor error fixes
    
    * format fix
    
    * AddDirectory to latest dev merge
    
    * format fix
    
    * name only std string to casacore string
    
    * remove old code
    
    * manually remove whitspace changes
    
    * format fix
    
    * more merge conflicts to align with dev
    
    * format fix
    
    * comment integration
    
    * remove irrelevant changes
    
    * Added SetDimensions and AddAxesNumber helper functions and implemented helper functions into required places
    
    * map and loop added
    
    * format fix
    
    * fix conditional for empty value
    
    * Fixed typo in header entry name and removed intermediate variable
    
    ---------
    
    Co-authored-by: Michaela van Zyl <michaela@maholla.com>
    Co-authored-by: Adrianna Pińska <adrianna.pinska@gmail.com>

--- src/ImageData/CompressedFits.cc
+++ src/ImageData/CompressedFits.cc
@@ -14,6 +14,7 @@
 #include "Timer/Timer.h"
 #include "Util/Casacore.h"
 #include "Util/FileSystem.h"
+#include "Util/Message.h"
 #include "Util/String.h"
 
 #define FITS_BLOCK_SIZE 2880
@@ -390,9 +391,8 @@ void CompressedFits::ParseFitsCard(
 void CompressedFits::AddHeaderEntry(
     casacore::String& keyword, casacore::String& value, casacore::String& comment, CARTA::FileInfoExtended& file_info_ext) {
     // Set CARTA::HeaderEntry fields in FileInfoExtended
-    auto entry = file_info_ext.add_header_entries();
-    entry->set_name(keyword);
-    *entry->mutable_value() = value;
+    CARTA::HeaderEntry* entry;
+    bool value_set(false);
 
     if (!value.empty()) {
         // Set type, numeric value
@@ -400,27 +400,28 @@ void CompressedFits::AddHeaderEntry(
             try {
                 // Set double value
                 double dvalue = std::stod(value);
-                entry->set_numeric_value(dvalue);
-                entry->set_entry_type(CARTA::EntryType::FLOAT);
+                entry = Message::AddHeaderEntry(file_info_ext, keyword, value, CARTA::EntryType::FLOAT, dvalue);
+                value_set = true;
             } catch (std::invalid_argument) {
-                // Set string value only
-                entry->set_entry_type(CARTA::EntryType::STRING);
             }
         } else {
             try {
                 // Set int value
                 int ivalue = std::stoi(value);
-                entry->set_numeric_value(ivalue);
-                entry->set_entry_type(CARTA::EntryType::INT);
+                entry = Message::AddHeaderEntry(file_info_ext, keyword, value, CARTA::EntryType::INT, ivalue);
+                value_set = true;
             } catch (std::invalid_argument) {
-                // Set string value only
-                entry->set_entry_type(CARTA::EntryType::STRING);
             } catch (std::out_of_range) {
                 long lvalue = std::stol(value);
-                entry->set_numeric_value(lvalue);
-                entry->set_entry_type(CARTA::EntryType::INT);
+                entry = Message::AddHeaderEntry(file_info_ext, keyword, value, CARTA::EntryType::INT, lvalue);
+                value_set = true;
             }
         }
+    }
+
+    if (!value_set) {
+        // Set string value only
+        entry = Message::AddHeaderEntry(file_info_ext, keyword, value);
     }
 
     if (!comment.empty()) {
