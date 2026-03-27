f856341a54485207b2219bd2fc156a3fda887aab
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2026-03-27 12:00:22 +0800

    style corrections for CI/CD to pass

--- src/ImageData/ADIOSImage.h
+++ src/ImageData/ADIOSImage.h
@@ -1,158 +1,144 @@
-#ifndef IMAGES_ADIOSIMAGE_H
-#define IMAGES_ADIOSIMAGE_H
+/* This file is part of the CARTA Image Viewer: https://github.com/CARTAvis/carta-backend
+   Copyright 2018- Academia Sinica Institute of Astronomy and Astrophysics (ASIAA),
+   Associated Universities, Inc. (AUI) and the Inter-University Institute for Data Intensive Astronomy (IDIA)
+   SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+#ifndef CARTA_SRC_IMAGEDATA_ADIOSIMAGE_H_
+#define CARTA_SRC_IMAGEDATA_ADIOSIMAGE_H_
 
 //# Includes
-#include <casacore/casa/aips.h>
-#include <casacore/images/Images/PagedImage.h>
-#include <casacore/images/Images/ImageInterface.h>
-#include <casacore/images/Images/ImageAttrHandlerCasa.h>
-#include <casacore/tables/Tables/Table.h>
 #include <casacore/casa/Utilities/DataType.h>
+#include <casacore/casa/aips.h>
+#include <casacore/images/Images/ImageAttrHandlerCasa.h>
+#include <casacore/images/Images/ImageInterface.h>
+#include <casacore/images/Images/PagedImage.h>
+#include <casacore/tables/Tables/Table.h>
 #include <casacore/tables/Tables/TableRecord.h>
 
 //# Forward Declarations
 #include <casacore/casa/iosfwd.h>
 
 #ifdef ADIOS2_USE_MPI
-#include <mpi.h>
-#include <askap/askapparallel/MPIComms.h>
 #include <askap/askapparallel/AskapParallel.h>
+#include <askap/askapparallel/MPIComms.h>
+#include <mpi.h>
 #endif
 
-template <class T> class ADIOSImage : public casacore::ImageInterface<T>
-{
+template <class T>
+class ADIOSImage : public casacore::ImageInterface<T> {
 public:
-
-  ADIOSImage ();
-/*  ~ADIOSImage() {
-     // temporary just to see when it is called ?
-     printf("In ~ADIOSImage ???\n");fflush(stdout);
-  }*/
-  ADIOSImage (
-    const casacore::TiledShape& mapShape,
-    const casacore::CoordinateSystem& coordinateInfo,
-    const casacore::String& nameOfNewFile,
-    casacore::String configname = "", 
-    casacore::uInt rowNumber = 0
-    );
+    ADIOSImage();
+    /*  ~ADIOSImage() {
+         // temporary just to see when it is called ?
+         printf("In ~ADIOSImage ???\n");fflush(stdout);
+      }*/
+    ADIOSImage(const casacore::TiledShape& mapShape, const casacore::CoordinateSystem& coordinateInfo,
+        const casacore::String& nameOfNewFile, casacore::String configname = "", casacore::uInt rowNumber = 0);
 
 #ifdef ADIOS2_USE_MPI
-  ADIOSImage (
-    askapparallel::AskapParallel &comms,
-    size_t comm_index,
-    const casacore::TiledShape& mapShape,
-    const casacore::CoordinateSystem& coordinateInfo,
-    const casacore::String& nameOfNewFile,
-    casacore::String configname = "", 
-    casacore::uInt rowNumber = 0
-    );
+    ADIOSImage(askapparallel::AskapParallel& comms, size_t comm_index, const casacore::TiledShape& mapShape,
+        const casacore::CoordinateSystem& coordinateInfo, const casacore::String& nameOfNewFile, casacore::String configname = "",
+        casacore::uInt rowNumber = 0);
 #endif
 
-  explicit ADIOSImage(
-    const casacore::String &filename,
-    casacore::String configname = "",
-    casacore::MaskSpecifier spec = casacore::MaskSpecifier(),
-    casacore::uInt rowNumber = 0);
+    explicit ADIOSImage(const casacore::String& filename, casacore::String configname = "",
+        casacore::MaskSpecifier spec = casacore::MaskSpecifier(), casacore::uInt rowNumber = 0);
 
 #ifdef ADIOS2_USE_MPI
-  explicit ADIOSImage(
-    askapparallel::AskapParallel &comms,
-    const casacore::String &filename,
-    casacore::String configname = "",
-    casacore::MaskSpecifier spec = casacore::MaskSpecifier(),
-    casacore::uInt rowNumber = 0);
+    explicit ADIOSImage(askapparallel::AskapParallel& comms, const casacore::String& filename, casacore::String configname = "",
+        casacore::MaskSpecifier spec = casacore::MaskSpecifier(), casacore::uInt rowNumber = 0);
 #endif
 
-  ADIOSImage (const ADIOSImage<T>& other);
+    ADIOSImage(const ADIOSImage<T>& other);
 
-  casacore::Table& table()
-    { return tab_p; }
+    casacore::Table& table() {
+        return tab_p;
+    }
 
-  static casacore::String className()
-    { return "ADIOSImage"; }
+    static casacore::String className() {
+        return "ADIOSImage";
+    }
 
-  virtual casacore::String imageType() const;
+    virtual casacore::String imageType() const;
 
-  void makeNewTable (const casacore::TiledShape& shape, casacore::uInt rowNumber, casacore::String filename);
+    void makeNewTable(const casacore::TiledShape& shape, casacore::uInt rowNumber, casacore::String filename);
 
-  virtual casacore::Bool setUnits (const casacore::Unit& newUnits);
+    virtual casacore::Bool setUnits(const casacore::Unit& newUnits);
 
-  virtual casacore::Bool setImageInfo(const casacore::ImageInfo& info);
+    virtual casacore::Bool setImageInfo(const casacore::ImageInfo& info);
 
-  virtual casacore::Bool setMiscInfo (const casacore::RecordInterface& newInfo);
+    virtual casacore::Bool setMiscInfo(const casacore::RecordInterface& newInfo);
 
-  virtual casacore::Bool setCoordinateInfo (const casacore::CoordinateSystem& coords);
+    virtual casacore::Bool setCoordinateInfo(const casacore::CoordinateSystem& coords);
 
-  virtual casacore::IPosition shape() const;
+    virtual casacore::IPosition shape() const;
 
-  virtual casacore::String name (casacore::Bool stripPath=casacore::False) const;
+    virtual casacore::String name(casacore::Bool stripPath = casacore::False) const;
 
-  virtual casacore::Bool ok() const;
+    virtual casacore::Bool ok() const;
 
-  virtual casacore::Bool doGetSlice (casacore::Array<T>& buffer, const casacore::Slicer& theSlice);
+    virtual casacore::Bool doGetSlice(casacore::Array<T>& buffer, const casacore::Slicer& theSlice);
 
-  virtual void doPutSlice (const casacore::Array<T>& sourceBuffer, const casacore::IPosition& where, const casacore::IPosition& stride);
+    virtual void doPutSlice(const casacore::Array<T>& sourceBuffer, const casacore::IPosition& where, const casacore::IPosition& stride);
 
-  virtual const casacore::LatticeRegion* getRegionPtr() const;
+    virtual const casacore::LatticeRegion* getRegionPtr() const;
 
-  virtual casacore::ImageInterface<T>* cloneII() const;
+    virtual casacore::ImageInterface<T>* cloneII() const;
 
-  virtual void resize (const casacore::TiledShape& newShape);
+    virtual void resize(const casacore::TiledShape& newShape);
 
-  virtual void reopenColumn();
+    virtual void reopenColumn();
 
-  virtual casacore::Bool isPaged() const;
+    virtual casacore::Bool isPaged() const;
 
-  virtual void setDefaultMask (const casacore::String& maskName);
+    virtual void setDefaultMask(const casacore::String& maskName);
 
-  virtual casacore::Lattice<casacore::Bool>& pixelMask();
+    virtual casacore::Lattice<casacore::Bool>& pixelMask();
 
-  //rewritten PagedImage private functions
-  void attach_logtable();
-  void setTableType();
-  void restoreAll (const casacore::TableRecord& rec);
-  void applyMaskSpecifier (const casacore::MaskSpecifier&);
-  void restoreImageInfo (const casacore::TableRecord& rec);
-  void restoreUnits (const casacore::TableRecord& rec);
-  void restoreMiscInfo (const casacore::TableRecord& rec);
-  void applyMask (const casacore::String& maskName);
-  static casacore::Table& getTable (void* imagePtr, casacore::Bool writable);
+    // rewritten PagedImage private functions
+    void attach_logtable();
+    void setTableType();
+    void restoreAll(const casacore::TableRecord& rec);
+    void applyMaskSpecifier(const casacore::MaskSpecifier&);
+    void restoreImageInfo(const casacore::TableRecord& rec);
+    void restoreUnits(const casacore::TableRecord& rec);
+    void restoreMiscInfo(const casacore::TableRecord& rec);
+    void applyMask(const casacore::String& maskName);
+    static casacore::Table& getTable(void* imagePtr, casacore::Bool writable);
 
 private:
-  void reopenRW();
+    void reopenRW();
 
-  casacore::ArrayColumn<T> map_p;
-  casacore::Table tab_p;
-  casacore::LatticeRegion* regionPtr_p;
-  casacore::uInt row_p;
-  casacore::String config;
-  // PJE - not certain how best to store the adios configuration besides
-  // a string containing the xml file of adios configuration
-  // 
+    casacore::ArrayColumn<T> map_p;
+    casacore::Table tab_p;
+    casacore::LatticeRegion* regionPtr_p;
+    casacore::uInt row_p;
+    casacore::String config;
+    // PJE - not certain how best to store the adios configuration besides
+    // a string containing the xml file of adios configuration
+    //
 
-  // for MPI lets store a default MPI_COMM_SELF communicator
+    // for MPI lets store a default MPI_COMM_SELF communicator
 #ifdef ADIOS2_USE_MPI
-  MPI_Comm adios_comm = MPI_COMM_SELF;
+    MPI_Comm adios_comm = MPI_COMM_SELF;
 #endif
 
-
 public:
-  using casacore::ImageInterface<T>::setMiscInfoMember;
-  using casacore::ImageInterface<T>::setUnitMember;
-  using casacore::ImageInterface<T>::imageInfo;
-  using casacore::ImageInterface<T>::setLogMember;
-  using casacore::ImageInterface<T>::setCoordsMember;
-  using casacore::ImageInterface<T>::setImageInfoMember;
-  using casacore::ImageInterface<T>::getDefaultMask;
-  using casacore::ImageInterface<T>::hasRegion;
-  using casacore::ImageInterface<T>::getImageRegionPtr;
-  using casacore::ImageInterface<T>::coordinates;
+    using casacore::ImageInterface<T>::setMiscInfoMember;
+    using casacore::ImageInterface<T>::setUnitMember;
+    using casacore::ImageInterface<T>::imageInfo;
+    using casacore::ImageInterface<T>::setLogMember;
+    using casacore::ImageInterface<T>::setCoordsMember;
+    using casacore::ImageInterface<T>::setImageInfoMember;
+    using casacore::ImageInterface<T>::getDefaultMask;
+    using casacore::ImageInterface<T>::hasRegion;
+    using casacore::ImageInterface<T>::getImageRegionPtr;
+    using casacore::ImageInterface<T>::coordinates;
 
-  using casacore::Lattice<T>::ndim;
-
+    using casacore::Lattice<T>::ndim;
 };
-
 
 #include "ADIOSImage.tcc"
 
-#endif
+#endif // CARTA_SRC_IMAGEDATA_ADIOSIMAGE_H_
--- src/ImageData/CasaLoader.h
+++ src/ImageData/CasaLoader.h
@@ -13,14 +13,13 @@
 #include <casacore/images/Images/TempImage.h>
 
 // need to discover the file type:
-#include <casacore/tables/Tables/Table.h>
 #include <casacore/casa/Containers/Record.h>
 #include <casacore/casa/OS/Directory.h>
 #include <casacore/casa/OS/File.h>
+#include <casacore/tables/Tables/Table.h>
 
-
-#include "FileLoader.h"
 #include "ADIOSImage.h"
+#include "FileLoader.h"
 
 namespace carta {
 
@@ -40,7 +39,8 @@ CasaLoader::CasaLoader(const std::string& filename) : FileLoader(filename) {}
 bool CasaLoader::isAdios2FormatSafe(const casacore::String& path) {
     // 1. Basic check: Is it even a directory?
     casacore::Directory dir(path);
-    if (!dir.exists()) return false;
+    if (!dir.exists())
+        return false;
 
     // 2. Look for the "table.info" file which is plain text
     casacore::File tableInfo(path + "/table.f0.bp/md.idx");
@@ -57,20 +57,21 @@ bool CasaLoader::isAdios2FormatSafe(const casacore::String& path) {
 
 bool CasaLoader::isAdios2Format(const casacore::String& path) {
     try {
-        // Use Table::Old to ensure read-only 
+        // Use Table::Old to ensure read-only
         // Use TableLock::NoLocking to avoid interfering with the ADIOS2 engine
         casacore::Table tab(path, casacore::TableLock(casacore::TableLock::NoLocking), casacore::Table::Old);
         // Look at the data manager info for the first column (usually 'map')
         casacore::Record dminfo = tab.dataManagerInfo();
-        
-        for (int i=0; i<dminfo.nfields(); ++i) {
+
+        for (int i = 0; i < dminfo.nfields(); ++i) {
             casacore::Record sub = dminfo.subRecord(i);
             if (sub.isDefined("TYPE") && sub.asString("TYPE") == "Adios2StMan") {
-                printf("It is ADIOS2 file!\n");fflush(stdout);
+                printf("It is ADIOS2 file!\n");
+                fflush(stdout);
                 return true;
             }
         }
-//        tab.close();
+        //        tab.close();
     } catch (...) {
         return false;
     }
@@ -91,39 +92,39 @@ void CasaLoader::AllocateImage(const std::string& /*hdu*/) {
         lock_file.reset(nullptr);
 
         bool converted(false);
-        
+
         try {
             bool isADIOS = isAdios2Format(_filename);
             // this logic should really be at a lower level in some CASA function that can read both formats:
             if (isADIOS) {
-                _image.reset(new ADIOSImage<float>(_filename));                
+                _image.reset(new ADIOSImage<float>(_filename));
                 // int blc=0,trc=0;
                 casacore::IPosition imageShape = _image->shape();
                 int ndim = imageShape.nelements();
 
                 // Create BLC and TRC with the correct number of dimensions
-                casacore::IPosition blc(ndim, 0); 
+                casacore::IPosition blc(ndim, 0);
                 casacore::IPosition trc(ndim, 0); // 0 ok
                 // const casacore::IPosition blc(0,0,0,0),trc(0,0,0,0);
                 const casacore::Slicer slicer(blc, trc, casacore::Slicer::endIsLast);
                 // casacore::Array<float> tempSlice(_image->getSlice(slicer).shape(), static_cast<float>(0.0));
-                // printf("TEST VALUE = %.8f\n",tempSlice[0]);                
+                // printf("TEST VALUE = %.8f\n",tempSlice[0]);
                 casacore::Array<float> tempSlice = _image->getSlice(casacore::Slicer(blc, trc, casacore::Slicer::endIsLast));
-                
+
                 std::cout << "Value at origin: " << tempSlice(casacore::IPosition(tempSlice.ndim(), 0)) << std::endl;
 
                 // 3. Alternatively, print the entire Array object (Casacore has a built-in logger)
                 std::cout << "Full slice contents: " << tempSlice << std::endl;
-                
+
                 // Define the specific coordinate
                 // Assuming a 2D image. If 4D, use casacore::IPosition(4, 158, 172, 0, 0)
-                int x=158,y=172;
-                //casacore::IPosition pos(2, x, y); 
+                int x = 158, y = 172;
+                // casacore::IPosition pos(2, x, y);
 
                 // Retrieve the single value
-                //float pixelValue = _image->getAt(pos);
-                //std::cout << "Value at (" << x << "," << y << ") = " << pixelValue << std::endl;
-                
+                // float pixelValue = _image->getAt(pos);
+                // std::cout << "Value at (" << x << "," << y << ") = " << pixelValue << std::endl;
+
                 casacore::IPosition coord(ndim, 0);
                 coord(0) = x; // X
                 coord(1) = y; // Y
@@ -135,9 +136,10 @@ void CasaLoader::AllocateImage(const std::string& /*hdu*/) {
                 // Accessing the result (the array is size 1, so index is 0)
                 std::cout << "Value via Slicer: " << pixelArray(casacore::IPosition(ndim, 0)) << std::endl;
             } else {
-                _image.reset(new casacore::PagedImage<float>(_filename));               
+                _image.reset(new casacore::PagedImage<float>(_filename));
             }
-            printf("DEBUG : read image %s using ADIOSImage<float> class, isADIOS=%d\n",_filename.c_str(),isADIOS);fflush(stdout);
+            printf("DEBUG : read image %s using ADIOSImage<float> class, isADIOS=%d\n", _filename.c_str(), isADIOS);
+            fflush(stdout);
         } catch (const casacore::AipsError& err) {
             printf("EXCEPTION CAUGHT !!!\n");
             if (err.getMesg().startsWith("Invalid Table data type")) {
