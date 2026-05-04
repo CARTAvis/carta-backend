4d890c4c65170a31210e4b1a4a40de7a3525c503
Michaela van Zyl <michaela@maholla.com>
2026-05-04 16:17:00 +0200

    initial simplified TestMain implementation

--- test/TestMain.cc
+++ test/TestMain.cc
@@ -4,13 +4,9 @@
    SPDX-License-Identifier: GPL-3.0-or-later
 */
 
-#include <string>
-
 #include <filesystem>
-
 #include <gtest/gtest.h>
 #include <omp.h>
-#include <cxxopts/cxxopts.hpp>
 
 #include "CommonTestUtilities.h"
 #include "Logger/Logger.h"
@@ -18,58 +14,35 @@
 #include "ThreadManager/ThreadManager.h"
 
 #define TASK_THREAD_COUNT 3
+namespace fs = std::filesystem;
 
 using namespace carta;
 
 int main(int argc, char** argv) {
-    // Set gtest environment
     testing::InitGoogleTest(&argc, argv);
     testing::AddGlobalTestEnvironment(new CartaEnvironment());
 
-    // Set global environment
-    int omp_threads(omp_get_num_procs());
-
-    cxxopts::Options options("carta-icd-test", "CARTA ICD test");
-
-    // clang-format off
-    options.add_options()
-        ("h,help", "print usage")
-        ("verbosity", "display verbose logging from this level",
-         cxxopts::value<int>()->default_value(std::to_string(0)), "<level>")
-        ("no_log", "do not log output to a log file", cxxopts::value<bool>()->default_value("true"))
-        ("log_performance", "enable performance debug logs", cxxopts::value<bool>()->default_value("false"))
-        ("log_protocol_messages", "enable protocol message debug logs", cxxopts::value<bool>()->default_value("false"))
-        ("t,omp_threads", "manually set OpenMP thread pool count",
-         cxxopts::value<int>()->default_value(std::to_string(omp_threads)), "<threads>");
-    // clang-format on
-
-    auto result = options.parse(argc, argv);
-
-    if (result.count("help")) {
-        std::cout << options.help() << std::endl;
-        return 0;
-    }
-
     auto& settings = ProgramSettings::GetInstance();
-    settings.verbosity = result["verbosity"].as<int>();
-    settings.no_log = result["no_log"].as<bool>();
-    settings.log_performance = result["log_performance"].as<bool>();
-    settings.log_protocol_messages = result["log_protocol_messages"].as<bool>();
-    omp_threads = result["omp_threads"].as<int>();
+    settings.no_log = true;
+    settings.verbosity = 0;
+    settings.log_performance = false;
+    settings.log_protocol_messages = false;
 
-    if (omp_threads < 0) {
-        omp_threads = omp_get_num_procs();
+    int omp_threads = omp_get_num_procs();
+    ThreadManager::StartEventHandlingThreads(TASK_THREAD_COUNT);
+    ThreadManager::SetThreadLimit(omp_threads);
+
+    const char* home = getenv("HOME");
+    if (home) {
+        settings.user_directory = fs::path(home) / CARTA_USER_FOLDER_PREFIX;
     }
+    
+    logger::InitLogger();
 
-    carta::ThreadManager::StartEventHandlingThreads(TASK_THREAD_COUNT);
-    carta::ThreadManager::SetThreadLimit(omp_threads);
-
-    ProgramSettings::GetInstance().user_directory = fs::path(getenv("HOME")) / CARTA_USER_FOLDER_PREFIX;
-    carta::logger::InitLogger();
-
-    int run_all_tests = RUN_ALL_TESTS();
+    int result = RUN_ALL_TESTS();
 
     ThreadManager::ExitEventHandlingThreads();
-    carta::logger::FlushLogFile();
-    return run_all_tests;
+    logger::FlushLogFile();
+    
+    return result;
 }
