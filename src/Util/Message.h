56000923baf258ed229e630aaf0c45f3d0554882
Michaela van Zyl <michaela@maholla.com>
2026-05-19 16:56:11 +0200

    format fix

--- src/Util/Message.h
+++ src/Util/Message.h
@@ -132,7 +132,8 @@ public:
     static CARTA::DirectoryInfo* AddDirectory(CARTA::FileListResponse& response, const std::string& name, time_t date, int item_count = 0);
     static CARTA::FileInfo* AddFile(CARTA::FileListResponse& response, std::string name, CARTA::FileType type = CARTA::FileType::UNKNOWN,
         int64_t size = 0, std::string hdu_list = "", int64_t date = 0);
-    static CARTA::DirectoryInfo* AddDirectory(CARTA::CatalogListResponse& response, const std::string& name, time_t date, int item_count = 0);
+    static CARTA::DirectoryInfo* AddDirectory(
+        CARTA::CatalogListResponse& response, const std::string& name, time_t date, int item_count = 0);
     static CARTA::CatalogFileInfo* AddFile(CARTA::CatalogListResponse& response, std::string name,
         CARTA::CatalogFileType type = CARTA::CatalogFileType::Unknown, int64_t size = 0, std::string description = "", int64_t date = 0);
     static CARTA::RegionListResponse RegionListResponse(CARTA::FileListResponse file_response);
