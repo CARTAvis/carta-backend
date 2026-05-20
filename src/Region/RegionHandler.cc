2ca396eb3f3faf05ff9e377f7d578de6f8b0a08c
Michaela van Zyl <michaela@maholla.com>
2026-05-20 13:21:36 +0200

    format fix

--- src/Region/RegionHandler.cc
+++ src/Region/RegionHandler.cc
@@ -188,7 +188,8 @@ void RegionHandler::ImportRegion(int file_id, std::shared_ptr<Frame> frame, CART
             _regions[region_id] = std::move(region);
             region_lock.unlock();
 
-            Message::AddImportedRegion(import_ack, region_id, props.state.type, {props.state.control_points.begin(), props.state.control_points.end()}, props.state.rotation, props.style);
+            Message::AddImportedRegion(import_ack, region_id, props.state.type,
+                {props.state.control_points.begin(), props.state.control_points.end()}, props.state.rotation, props.style);
             region_id += 1;
 
             success = true; // if any regions were set
