83bed7f4f4dd78826de5fec7e04d5ee49b460fad
Zhen-Kai Gao <zkgao@asiaa.sinica.edu.tw>
2026-06-16 17:20:10 +0800

    Configure TensorStore to use CARTA's vendored nlohmann_json library

--- third-party/install/TensorStore.cmake
+++ third-party/install/TensorStore.cmake
@@ -2,8 +2,15 @@ include(FetchContent)
 
 set(TENSORSTORE_USE_SYSTEM_ZLIB ON CACHE BOOL "" FORCE)
 set(TENSORSTORE_USE_SYSTEM_ZSTD ON CACHE BOOL "" FORCE)
-set(TENSORSTORE_USE_SYSTEM_NLOHMANN_JSON ON CACHE BOOL "" FORCE)
 set(TENSORSTORE_USE_SYSTEM_PROTOBUF OFF CACHE BOOL "" FORCE)
+
+# Point TensorStore at CARTA's vendored nlohmann_json instead of requiring a system install.
+set(TENSORSTORE_USE_SYSTEM_NLOHMANN_JSON ON CACHE BOOL "" FORCE)
+file(WRITE "${CMAKE_FIND_PACKAGE_REDIRECTS_DIR}/nlohmann_json-config.cmake"
+    "if(NOT TARGET nlohmann_json::nlohmann_json)\n"
+    "    message(FATAL_ERROR \"nlohmann_json redirect: target missing; add_subdirectory(third-party/nlohmann_json) must run before include(TensorStore)\")\n"
+    "endif()\n"
+    "set(nlohmann_json_FOUND TRUE)\n")
 
 FetchContent_Declare(
     tensorstore
